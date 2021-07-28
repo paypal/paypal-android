700c88891e97b6c94d4449ebc607414bd4280cbe
Steven <sshropshire@paypal.com>
2021-07-28 16:31:11 -0500

    Rename MainActivity to DemoActivity.

--- Demo/src/main/java/com/paypal/android/DemoActivity.kt
+++ Demo/src/main/java/com/paypal/android/DemoActivity.kt
@@ -3,7 +3,7 @@ package com.paypal.android
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class DemoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
