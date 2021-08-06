cad56eb9e57bc3d0e41f1941a93f90f23070471f
Koop <skoop@paypal.com>
2021-08-06 11:00:01 -0500

    Remove upload artifact

--- Card/src/main/java/com/paypal/android/card/CardClient.kt
+++ Card/src/main/java/com/paypal/android/card/CardClient.kt
@@ -1,6 +1,6 @@
 package com.paypal.android.card
 
-import com.paypal.android.core.*
+import com.paypal.android.core.PaymentsClient
 
 @Suppress("UnusedPrivateMember")
 class CardClient(paymentsClient: PaymentsClient)
