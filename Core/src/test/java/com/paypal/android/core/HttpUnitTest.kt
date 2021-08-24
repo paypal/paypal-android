daab8b242ff0791b4a9cf973da5d1f1276514860
Steven <sshropshire@paypal.com>
2021-08-24 15:34:49 -0500

    Implement additional unit test for http response.

--- Core/src/test/java/com/paypal/android/core/HttpUnitTest.kt
+++ Core/src/test/java/com/paypal/android/core/HttpUnitTest.kt
@@ -7,6 +7,7 @@ import io.mockk.verify
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.TestCoroutineDispatcher
 import kotlinx.coroutines.test.runBlockingTest
+import org.junit.Assert.assertEquals
 import org.junit.Test
 import java.net.HttpURLConnection
 import java.net.URL
@@ -31,5 +32,23 @@ class HttpUnitTest {
         sut.send(httpRequest, testCoroutineDispatcher)
 
         verify { urlConnection.requestMethod = "GET" }
+    }
+
+    @ExperimentalCoroutinesApi
+    @Test
+    fun `send returns an http result`() = runBlockingTest {
+        val httpRequest = spyk(HttpRequest("https://www.example.com"))
+
+        val url = mockk<URL>()
+        every { httpRequest.url } returns url
+
+        val urlConnection = mockk<HttpURLConnection>(relaxed = true)
+        every { url.openConnection() } returns urlConnection
+        every { urlConnection.responseCode } returns 123
+
+        val sut = Http()
+        val result = sut.send(httpRequest, testCoroutineDispatcher)
+
+        assertEquals(123, result.responseCode)
     }
 }
