ca828e1d3bf0f7fd3a9160ab1fa42e3efcce1b9e
Jose Noriega <jonoriega@paypal.com>
2021-09-20 17:54:01 -0300

    First card client approach

--- Card/src/main/java/com/paypal/android/card/CardClient.kt
+++ Card/src/main/java/com/paypal/android/card/CardClient.kt
@@ -1,20 +1,37 @@
 package com.paypal.android.card
 
-import com.paypal.android.card.network.CardAPIClient
 import com.paypal.android.core.*
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import java.net.URL
+import kotlin.coroutines.CoroutineContext
 
-
-class CardClient(val paymentConfig: PaymentsConfiguration) {
-
-    private val apiClient = CardAPIClient()
+class CardClient(val paymentsClient: PaymentsClient) {
 
     fun approveOrder(orderId: String, card: Card, completion: (CardResult) -> Unit) {
+        val url = "/v2/checkout/orders/${orderId}/confirm-payment-source"
+        val body = """
+            {
+                "payment_source": {
+                    "card": {
+                        "number":"${card.number}",
+                        "expiry":"${card.expiry}"
+                    }
+                }
+            }
+        """.trimIndent()
+        val finalURL = Environment.SANDBOX.url + url;
+        val request = HttpRequest(URL(finalURL), "POST", body)
+        val http = Http()
         CoroutineScope(Dispatchers.Main).launch {
-            val cardResult = apiClient.confirmPaymentSource(paymentConfig, orderId, card)
-            completion(cardResult)
+            val response = http.send(request)
+            //response to CardResult
+            //
+            completion(CardResult(null, Object()))
         }
+
+
+        //paymentsClients.approveOrder(orderId, PaymentSource(card))
     }
 }
--- Core/src/main/java/com/paypal/android/core/HttpRequest.kt
+++ Core/src/main/java/com/paypal/android/core/HttpRequest.kt
@@ -3,8 +3,5 @@ package com.paypal.android.core
 import java.net.URL
 
 data class HttpRequest(val url: URL, val method: String, val body: String) {
-
-    val headers = mutableMapOf<String, String>()
-
     constructor(url: String) : this(URL(url), "GET", "")
 }
