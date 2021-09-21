f72eb4c239c7fdce94d2e6eabc53b2dd826430e3
Koop <skoop@paypal.com>
2021-09-21 14:39:52 -0500

    Cleanup unused code

--- Demo/src/main/java/com/paypal/android/ui/PaymentMethodsFragment.kt
+++ Demo/src/main/java/com/paypal/android/ui/PaymentMethodsFragment.kt
@@ -21,11 +21,6 @@ import com.paypal.android.ui.card.CardFields
 import com.paypal.android.ui.card.CardViewModel
 import com.paypal.android.ui.theme.DemoTheme
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
 /**
  * A simple [Fragment] subclass.
  * Use the [PaymentMethodsFragment.newInstance] factory method to
