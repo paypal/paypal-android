54a423eff2c05eb18132bb677565e28d7ea7ec93
Steven <sshropshire@paypal.com>
2021-09-23 16:14:59 -0500

    Remove additional imports.

--- Card/src/test/java/com/paypal/android/card/CardClientUnitTest.kt
+++ Card/src/test/java/com/paypal/android/card/CardClientUnitTest.kt
@@ -1,7 +1,5 @@
 package com.paypal.android.card
 
-import com.paypal.android.card.network.CardAPIClient
-import com.paypal.android.core.APIClient
 import com.paypal.android.core.PaymentsConfiguration
 import org.junit.Assert.assertNotNull
 import org.junit.Test
