ecb54704ddf878ec5a178dc78f823d5b4f1d2112
Steven <sshropshire@paypal.com>
2021-09-27 14:31:44 -0500

    Add CardClient documentation.

--- Card/src/main/java/com/paypal/android/card/CardClient.kt
+++ Card/src/main/java/com/paypal/android/card/CardClient.kt
@@ -7,10 +7,20 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
-class CardClient(configuration: PaymentsConfiguration) {
-
+/**
+ * Use this client to approve an order with a [Card].
+ */
+class CardClient(
+    configuration: PaymentsConfiguration,
     private val cardAPI: CardAPIClient = CardAPIClient(APIClient(configuration))
+) {
 
+    /**
+     * Approve an order with a [Card].
+     * @param orderID The id of the order
+     * @param card The card to use for approval
+     * @param completion A completion handler for receiving a [CardResult]
+     */
     fun approveOrder(orderID: String, card: Card, completion: (CardResult) -> Unit) {
         CoroutineScope(Dispatchers.Main).launch {
             val cardResult = cardAPI.confirmPaymentSource(orderID, card)
