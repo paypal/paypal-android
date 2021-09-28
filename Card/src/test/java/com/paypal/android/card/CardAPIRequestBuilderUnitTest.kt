1d8ab0fae8a97d1db61e370397868a4e0e23c6d6
Steven <sshropshire@paypal.com>
2021-09-28 11:18:56 -0500

    Write test for CardAPIRequestBuilder.

---
+++ Card/src/test/java/com/paypal/android/card/CardAPIRequestBuilderUnitTest.kt
@@ -0,0 +1,42 @@
+package com.paypal.android.card
+
+import io.mockk.every
+import io.mockk.mockk
+import org.json.JSONObject
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+
+@RunWith(RobolectricTestRunner::class)
+class CardAPIRequestBuilderUnitTest {
+
+    private val orderID = "sample-order-id"
+    private val card = Card("4111 1111 1111 1111", "01/22", "123")
+
+    private val dateParser = mockk<DateParser>()
+
+    private lateinit var sut: CardAPIRequestBuilder
+
+    @Before
+    fun beforeEach() {
+        sut = CardAPIRequestBuilder(dateParser)
+    }
+
+    @Test
+    fun `it builds a confirm payment source request`() {
+        val cardExpiry = CardExpiry("01", "2022")
+        every { dateParser.parseCardExpiry("01/22") } returns cardExpiry
+
+        val apiRequest = sut.buildConfirmPaymentSourceRequest(orderID, card)
+        assertEquals("v2/checkout/orders/sample-order-id/confirm-payment-source", apiRequest.path)
+
+        val json = JSONObject(apiRequest.body!!)
+        val paymentSource = json.getJSONObject("payment_source")
+        val card = paymentSource.getJSONObject("card")
+        assertEquals("4111111111111111", card.getString("number"))
+        assertEquals("2022-01", card.getString("expiry"))
+        assertEquals("123", card.getString("security_code"))
+    }
+}
