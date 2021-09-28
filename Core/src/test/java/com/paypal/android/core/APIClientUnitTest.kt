f007d53c56c033ccd602a8ca873ca43235424759
Steven <sshropshire@paypal.com>
2021-09-28 13:19:11 -0500

    Implement APIClient unit test.

--- Core/src/test/java/com/paypal/android/core/APIClientUnitTest.kt
+++ Core/src/test/java/com/paypal/android/core/APIClientUnitTest.kt
@@ -1,8 +1,21 @@
 package com.paypal.android.core
 
-import org.junit.Assert.assertNotNull
+import io.mockk.coEvery
+import io.mockk.every
+import io.mockk.mockk
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.test.TestCoroutineDispatcher
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.setMain
+import org.junit.After
+import org.junit.Assert.assertSame
+import org.junit.Before
 import org.junit.Test
+import java.net.URL
 
+@ExperimentalCoroutinesApi
 class APIClientUnitTest {
 
     companion object {
@@ -10,9 +23,44 @@ class APIClientUnitTest {
         private const val CLIENT_SECRET = "sample-client-secret"
     }
 
+    private val http = mockk<Http>()
+    private val httpRequestFactory = mockk<HttpRequestFactory>()
+
+    private val apiRequest = APIRequest("/sample/path", HttpMethod.GET, null)
+    private val configuration = PaymentsConfiguration(CLIENT_ID, CLIENT_SECRET)
+
+    private val testCoroutineDispatcher = TestCoroutineDispatcher()
+
+    private lateinit var sut: APIClient
+
+    @Before
+    fun beforeEach() {
+        sut = APIClient(configuration, http, httpRequestFactory)
+
+        Dispatchers.setMain(testCoroutineDispatcher)
+    }
+
+    @After
+    fun afterEach() {
+        Dispatchers.resetMain()
+        testCoroutineDispatcher.cleanupTestCoroutines()
+    }
+
     @Test
-    fun constructor() {
-        val sut = APIClient(PaymentsConfiguration(CLIENT_ID, CLIENT_SECRET))
-        assertNotNull(sut)
+    fun `converts an api request to an http request and sends it`() = runBlocking {
+        val url = URL("https://example.com/resolved/path")
+
+        val httpRequest = HttpRequest(url, HttpMethod.GET)
+        every {
+            httpRequestFactory.createHttpRequestFromAPIRequest(apiRequest, configuration)
+        } returns httpRequest
+
+        val httpResponse = HttpResponse(200)
+        coEvery {
+            http.send(httpRequest)
+        } returns httpResponse
+
+        val result = sut.send(apiRequest)
+        assertSame(httpResponse, result)
     }
 }
