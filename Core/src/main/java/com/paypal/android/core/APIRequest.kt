79664bf6e823e088c85a3e14733bfc1535dcab6c
Steven <sshropshire@paypal.com>
2021-09-28 09:57:04 -0500

    Cleanup APIRequest.

--- Core/src/main/java/com/paypal/android/core/APIRequest.kt
+++ Core/src/main/java/com/paypal/android/core/APIRequest.kt
@@ -1,9 +1,3 @@
 package com.paypal.android.core
 
-data class APIRequest(val path: String, val method: HttpMethod, val body: String?) {
-
-    var contentType: HttpContentType = HttpContentType.JSON
-
-    private val _headers: MutableMap<String, String> = mutableMapOf()
-    val headers: Map<String, String> = _headers
-}
+data class APIRequest(val path: String, val method: HttpMethod, val body: String?)
