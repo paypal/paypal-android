4ba110f8304efeea39b6b251ce92e26f7aa37f58
Steven <sshropshire@paypal.com>
2021-10-01 16:49:51 -0500

    Remove wildcard imports.

--- Demo/src/main/java/com/paypal/android/ui/card/CardViewModel.kt
+++ Demo/src/main/java/com/paypal/android/ui/card/CardViewModel.kt
@@ -6,7 +6,11 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.paypal.android.BuildConfig
-import com.paypal.android.api.model.*
+import com.paypal.android.api.model.Amount
+import com.paypal.android.api.model.Order
+import com.paypal.android.api.model.Payee
+import com.paypal.android.api.model.CreateOrderRequest
+import com.paypal.android.api.model.PurchaseUnit
 import com.paypal.android.api.services.PayPalDemoApi
 import com.paypal.android.card.Card
 import com.paypal.android.card.CardClient
