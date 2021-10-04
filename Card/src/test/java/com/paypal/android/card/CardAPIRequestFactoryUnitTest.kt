5bc755927bdb7de502776c2f90e535ba54048f80
Steven <sshropshire@paypal.com>
2021-10-04 14:25:13 -0500

    Remove unecessary tests.

--- Card/src/test/java/com/paypal/android/card/CardAPIRequestFactoryUnitTest.kt
+++ Card/src/test/java/com/paypal/android/card/CardAPIRequestFactoryUnitTest.kt
@@ -1,7 +1,5 @@
 package com.paypal.android.card
 
-import io.mockk.every
-import io.mockk.mockk
 import org.json.JSONObject
 import org.junit.Assert.assertEquals
 import org.junit.Before
@@ -13,21 +11,17 @@ import org.robolectric.RobolectricTestRunner
 class CardAPIRequestFactoryUnitTest {
 
     private val orderID = "sample-order-id"
-    private val card = Card("4111 1111 1111 1111", "01/22", "123")
-
-    private val dateParser = mockk<com.paypal.android.ui.card.DateParser>()
+    private val card = Card("4111 1111 1111 1111", "01", "2022", "123")
 
     private lateinit var sut: CardAPIRequestFactory
 
     @Before
     fun beforeEach() {
-        sut = CardAPIRequestFactory(dateParser)
+        sut = CardAPIRequestFactory()
     }
 
     @Test
     fun `it builds a confirm payment source request`() {
-        val cardExpiry = com.paypal.android.ui.card.ExpirationDate(1, 2022)
-        every { dateParser.parseExpirationDate("01/22") } returns cardExpiry
 
         val apiRequest = sut.createConfirmPaymentSourceRequest(orderID, card)
         assertEquals("v2/checkout/orders/sample-order-id/confirm-payment-source", apiRequest.path)
