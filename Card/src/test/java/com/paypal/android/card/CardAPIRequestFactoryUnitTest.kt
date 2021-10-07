7caea0fb73f466ca75eb0c582c4149444e7721fa
Andres Pelaez <anpelaez@paypal.com>
2021-10-07 15:15:39 -0500

    Add card confirm-payment-source test omitting optional params.
    Signed-off-by: Steven Shropshire <steven.shropshire@getbraintree.com>

--- Card/src/test/java/com/paypal/android/card/CardAPIRequestFactoryUnitTest.kt
+++ Card/src/test/java/com/paypal/android/card/CardAPIRequestFactoryUnitTest.kt
@@ -6,6 +6,7 @@ import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
+import org.skyscreamer.jsonassert.JSONAssert
 
 @RunWith(RobolectricTestRunner::class)
 class CardAPIRequestFactoryUnitTest {
@@ -32,5 +33,26 @@ class CardAPIRequestFactoryUnitTest {
         assertEquals("4111111111111111", card.getString("number"))
         assertEquals("2022-01", card.getString("expiry"))
         assertEquals("123", card.getString("security_code"))
+    }
+
+    @Test
+    fun `it omits optional params when they are not set`() {
+        val card = Card("4111111111111111", "01", "2022")
+
+        val apiRequest = sut.createConfirmPaymentSourceRequest(orderID, card)
+        assertEquals("v2/checkout/orders/sample-order-id/confirm-payment-source", apiRequest.path)
+
+        // language=JSON
+        val expectedJSON = """
+            {
+              "payment_source": {
+                "card": {
+                  "number": "4111111111111111",
+                  "expiry": "2022-01"
+                }
+              }
+            }
+        """.trimIndent()
+        JSONAssert.assertEquals(JSONObject(expectedJSON), JSONObject(apiRequest.body!!), true)
     }
 }
