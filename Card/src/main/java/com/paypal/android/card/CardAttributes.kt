67c4db6eb551dcbedd69a475e626a95fffd7dc34
Steven <sshropshire@paypal.com>
2021-10-11 15:57:28 -0500

    Remove CardAttributes class.

--- Card/src/main/java/com/paypal/android/card/CardAttributes.kt
+++
@@ -1,8 +0,0 @@
-package com.paypal.android.card
-
-data class CardAttributes(
-    val customerId: String? = null,
-    val customerEmail: String? = null,
-    val vaultOnOrderCompletion: Boolean = false,
-    val verificationMethod: CardVerificationMethod? = null
-)
