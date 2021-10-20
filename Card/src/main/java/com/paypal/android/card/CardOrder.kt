957b3bc99298d01ae1a4bea348459ceb50745fd0
Tim Chow <tichow@paypal.com>
2021-10-20 15:46:48 -0500

    Remove unused classes

--- Card/src/main/java/com/paypal/android/card/CardOrder.kt
+++
@@ -1,9 +0,0 @@
-package com.paypal.android.card
-
-import com.paypal.android.core.OrderData
-import com.paypal.android.core.OrderStatus
-
-data class CardOrder(
-    override val orderID: String,
-    override val status: OrderStatus
-) : OrderData
--- Core/src/main/java/com/paypal/android/core/OrderData.kt
+++
@@ -1,6 +0,0 @@
-package com.paypal.android.core
-
-interface OrderData {
-    val orderID: String
-    val status: OrderStatus
-}
