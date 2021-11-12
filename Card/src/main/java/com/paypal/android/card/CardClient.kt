b613ae21c5688ee1e0e78d5c05795fbcd939e3bd
Steven <sshropshire@paypal.com>
2021-11-12 10:40:15 -0600

    Fix detekt errors.

--- Card/src/main/java/com/paypal/android/card/CardClient.kt
+++ Card/src/main/java/com/paypal/android/card/CardClient.kt
@@ -2,10 +2,6 @@ package com.paypal.android.card
 
 import com.paypal.android.core.API
 import com.paypal.android.core.CoreConfig
-import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 
 /**
  * Use this client to approve an order with a [Card].
@@ -20,7 +16,7 @@ class CardClient internal constructor(private val cardAPI: CardAPI) {
      *
      * @param orderID The id of the order
      * @param card The card to use for approval
-     * @param completion A completion handler for receiving a [CardResult]
+     * @return [CardResult]
      */
     suspend fun approveOrder(orderID: String, card: Card) =
         cardAPI.confirmPaymentSource(orderID, card)
