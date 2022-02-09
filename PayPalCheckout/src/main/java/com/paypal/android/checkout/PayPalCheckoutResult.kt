5f4d15db34981b9dd3fe406dc2a581bcc4cde548
sshropshire <sshropshire@paypal.com>
2022-02-09 11:44:12 -0600

    Remove PayPalCheckoutResult.Cancelled type.

--- PayPalCheckout/src/main/java/com/paypal/android/checkout/PayPalCheckoutResult.kt
+++ PayPalCheckout/src/main/java/com/paypal/android/checkout/PayPalCheckoutResult.kt
@@ -5,5 +5,4 @@ import com.paypal.android.checkout.pojo.ErrorInfo
 sealed class PayPalCheckoutResult {
     class Success(val orderId: String?, val payerId: String?) : PayPalCheckoutResult()
     class Failure(val error: ErrorInfo) : PayPalCheckoutResult()
-    object Cancellation : PayPalCheckoutResult()
 }
