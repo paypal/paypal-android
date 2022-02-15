bbbf7df3be0b2e82df4ac48a930baed100393a81
sshropshire <sshropshire@paypal.com>
2022-02-15 13:37:53 -0600

    Make PayPal class internal and add doc string to PayPalCheckoutResult.

--- PayPalCheckout/src/main/java/com/paypal/android/checkout/PayPalCheckoutResult.kt
+++ PayPalCheckout/src/main/java/com/paypal/android/checkout/PayPalCheckoutResult.kt
@@ -1,3 +1,6 @@
 package com.paypal.android.checkout
 
+/**
+ * A result passed to a [PayPalListener] when the PayPal flow completes successfully.
+ */
 data class PayPalCheckoutResult(val orderId: String?, val payerId: String?)
