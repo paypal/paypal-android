b510adcf161df2bb4706e8bb933a61e5f68edbd3
sshropshire <sshropshire@paypal.com>
2022-02-15 13:31:55 -0600

    Add docs for PayPalListener.

--- PayPalCheckout/src/main/java/com/paypal/android/checkout/PayPalListener.kt
+++ PayPalCheckout/src/main/java/com/paypal/android/checkout/PayPalListener.kt
@@ -2,8 +2,25 @@ package com.paypal.android.checkout
 
 import com.paypal.android.core.PayPalSDKError
 
+/**
+ * Implement this callback to receive results from [PayPalClient].
+ */
 interface PayPalListener {
+
+    /**
+     * Called when the PayPal flow completes successfully.
+     * @param result [PayPalCheckoutResult] with order information.
+     */
     fun onPayPalSuccess(result: PayPalCheckoutResult)
+
+    /**
+     * Called when the PayPal flow completes with an error.
+     * @param error [PayPalSDKError] explaining the reason for failure.
+     */
     fun onPayPalFailure(error: PayPalSDKError)
+
+    /**
+     * Called when the PayPal flow was canceled by the user.
+     */
     fun onPayPalCanceled()
 }
