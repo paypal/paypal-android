8b746b59f774ced8311d54f9986b4571e2ffd09b
sshropshire <sshropshire@paypal.com>
2022-02-15 14:44:46 -0600

    Add docstring to PayPalSDKError.

--- Core/src/main/java/com/paypal/android/core/PayPalSDKError.kt
+++ Core/src/main/java/com/paypal/android/core/PayPalSDKError.kt
@@ -1,5 +1,8 @@
 package com.paypal.android.core
 
+/**
+ * Class used to describe PayPal errors when they occur.
+ */
 data class PayPalSDKError(
     val code: Int,
     val errorDescription: String,
