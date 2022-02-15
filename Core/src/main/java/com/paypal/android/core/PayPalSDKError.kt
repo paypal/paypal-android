1e89079f8646fcdcde8fd2d3d6030c9c30da7e7e
sshropshire <sshropshire@paypal.com>
2022-02-15 13:50:17 -0600

    Make code and errorDescription params required in PayPalSDKError.

--- Core/src/main/java/com/paypal/android/core/PayPalSDKError.kt
+++ Core/src/main/java/com/paypal/android/core/PayPalSDKError.kt
@@ -1,7 +1,7 @@
 package com.paypal.android.core
 
 data class PayPalSDKError(
-    val code: Int?,
-    val errorDescription: String?,
+    val code: Int,
+    val errorDescription: String,
     val correlationID: String? = null
 ) : Exception("Error: $code - Description: $errorDescription")
