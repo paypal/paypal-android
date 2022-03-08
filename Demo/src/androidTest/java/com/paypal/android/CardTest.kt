459ef5ea1be0e21638baddff6c22cb98da6616c7
sshropshire <sshropshire@paypal.com>
2022-03-08 11:56:52 -0600

    Rename ExampleInstrumentedTest to CardTest.

--- Demo/src/androidTest/java/com/paypal/android/CardTest.kt
+++ Demo/src/androidTest/java/com/paypal/android/CardTest.kt
@@ -6,13 +6,9 @@ import org.junit.Assert.assertEquals
 import org.junit.Test
 import org.junit.runner.RunWith
 
-/**
- * Instrumented test, which will execute on an Android device.
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
 @RunWith(AndroidJUnit4::class)
-class ExampleInstrumentedTest {
+class CardTest {
+
     @Test
     fun useAppContext() {
         // Context of the app under test.
