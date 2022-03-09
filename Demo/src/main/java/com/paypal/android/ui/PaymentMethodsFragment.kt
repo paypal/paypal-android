84ef443d2da9bc70c3a9d10d0b11f95ad8190c95
sshropshire <sshropshire@paypal.com>
2022-03-09 11:17:43 -0600

    Migrate PaymentMethodsFragment to xml layout.

--- Demo/src/main/java/com/paypal/android/ui/PaymentMethodsFragment.kt
+++ Demo/src/main/java/com/paypal/android/ui/PaymentMethodsFragment.kt
@@ -1,28 +1,16 @@
 package com.paypal.android.ui
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.Menu
-import android.view.MenuInflater
-import android.view.MenuItem
-import android.view.View
-import android.view.ViewGroup
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Button
-import androidx.compose.material.Text
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.ComposeView
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.dp
+import android.view.*
 import androidx.fragment.app.Fragment
 import androidx.navigation.NavDirections
 import androidx.navigation.fragment.findNavController
 import com.paypal.android.R
-import com.paypal.android.ui.theme.DemoTheme
+import com.paypal.android.databinding.FragmentPaymentMethodsBinding
 
 class PaymentMethodsFragment : Fragment() {
+
+    private lateinit var binding: FragmentPaymentMethodsBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,26 +22,13 @@ class PaymentMethodsFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return ComposeView(requireContext()).apply {
-            setContent {
-                DemoTheme {
-                    Column {
-                        Button(
-                            onClick = { launchCardFragment() },
-                            modifier = Modifier
-                                .padding(start = 16.dp, end = 16.dp, bottom = 16.dp, top = 16.dp)
-                                .fillMaxWidth()
-                        ) { Text(stringResource(R.string.payment_methods_card)) }
-                        Button(
-                            onClick = { launchPayPalFragment() },
-                            modifier = Modifier
-                                .padding(start = 16.dp, end = 16.dp, bottom = 16.dp)
-                                .fillMaxWidth()
-                        ) { Text(stringResource(R.string.payment_methods_paypal)) }
-                    }
-                }
-            }
+        binding = FragmentPaymentMethodsBinding.inflate(inflater, container, false)
+
+        binding.run {
+            cardButton.setOnClickListener { launchCardFragment() }
+            payPalButton.setOnClickListener { launchPayPalFragment() }
         }
+        return binding.root
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
