4597760911e3c91ce08d392eaf0abdc85d23a76d
sshropshire <sshropshire@paypal.com>
2022-03-10 11:54:01 -0600

    Fix autoformatting on CardFragment.

--- Demo/src/main/java/com/paypal/android/ui/card/CardFragment.kt
+++ Demo/src/main/java/com/paypal/android/ui/card/CardFragment.kt
@@ -2,27 +2,51 @@ package com.paypal.android.ui.card
 
 import android.os.Bundle
 import android.text.Editable
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import com.paypal.android.BuildConfig
 import com.paypal.android.R
+import com.paypal.android.api.model.*
+import com.paypal.android.api.services.PayPalDemoApi
+import com.paypal.android.card.Card
+import com.paypal.android.card.CardClient
+import com.paypal.android.card.CardRequest
+import com.paypal.android.core.CoreConfig
+import com.paypal.android.core.PayPalSDKError
 import com.paypal.android.databinding.FragmentCardBinding
-import com.paypal.android.text.SimpleTextWatcher
+import com.paypal.android.text.ValueChangeWatcher
+import com.paypal.android.text.onValueChange
 import com.paypal.android.ui.card.validation.CardFormatter
 import com.paypal.android.utils.SharedPreferenceUtil
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @AndroidEntryPoint
 class CardFragment : Fragment() {
 
-    private lateinit var binding: FragmentCardBinding
+    companion object {
+        private val TAG = CardFragment::class.qualifiedName
+    }
 
     @Inject
     lateinit var preferenceUtil: SharedPreferenceUtil
+
+    @Inject
+    lateinit var payPalDemoApi: PayPalDemoApi
+
+    private lateinit var binding: FragmentCardBinding
+
+    private val configuration =
+        CoreConfig(BuildConfig.CLIENT_ID, clientSecret = BuildConfig.CLIENT_SECRET)
+    private val cardClient = CardClient(configuration)
+
     private val cardViewModel: CardViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -36,7 +60,17 @@ class CardFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentCardBinding.inflate(inflater, container, false)
-        initializeDropDown()
+
+        binding.run {
+            autoCompleteTextView.setAdapter(createPrefillCardsAdapter())
+
+            autoCompleteTextView.onValueChange = ::onPrefillCardValueChange
+            cardNumberInput.onValueChange = ::onCardNumberValueChange
+            cardExpirationInput.onValueChange = ::onCardExpirationValueChange
+
+            submitButton.setOnClickListener { onCardFieldSubmit() }
+        }
+
         return binding.root
 //        return ComposeView(requireContext()).apply {
 //            setContent {
@@ -64,6 +98,25 @@ class CardFragment : Fragment() {
 //        }
     }
 
+    private fun onPrefillCardValueChange(oldValue: String, newValue: String) {
+
+    }
+
+    private fun onCardNumberValueChange(oldValue: String, newValue: String) {
+        val formattedCardNumber = CardFormatter.formatCardNumber(newValue, oldValue)
+        binding.cardNumberInput.setText(formattedCardNumber)
+        binding.cardNumberInput.setSelection(formattedCardNumber.length)
+    }
+
+    private fun onCardExpirationValueChange(oldValue: String, newValue: String) {
+
+    }
+
+    private fun createPrefillCardsAdapter(): ArrayAdapter<String> {
+        val autoFillCardNames = cardViewModel.autoFillCards.map { it.first }
+        return ArrayAdapter(requireActivity(), R.layout.dropdown_item, autoFillCardNames)
+    }
+
     private fun initializeDropDown() {
         // set drop down menu items
         val autoFillCardNames = cardViewModel.autoFillCards.map { it.first }
@@ -71,11 +124,9 @@ class CardFragment : Fragment() {
 
         binding.run {
             autoCompleteTextView.setAdapter(adapter)
-            autoCompleteTextView.addTextChangedListener(object: SimpleTextWatcher() {
-                override fun afterTextChanged(p0: Editable?) {
-                    cardViewModel.selectedPrefillCard.value = autoCompleteTextView.text.toString()
-                }
-            })
+//            autoCompleteTextView.onValueChange = {
+//                cardViewModel.selectedPrefillCard.value = autoCompleteTextView.text.toString()
+//            }
         }
 
         cardViewModel.selectedPrefillCard.observe(viewLifecycleOwner) { cardName ->
@@ -88,5 +139,56 @@ class CardFragment : Fragment() {
                 }
             }
         }
+    }
+
+    private fun onCardFieldSubmit() {
+        val cardNumber = CardFormatter.formatCardNumber(binding.cardNumberInput.text.toString())
+        val expirationDate = binding.cardExpirationInput.text.toString()
+        val securityCode = binding.cardSecurityCodeInput.text.toString()
+
+        Log.d(TAG, "$cardNumber")
+        Log.d(TAG, "$expirationDate")
+        Log.d(TAG, "$securityCode")
+        Log.d(TAG, "Environment = ${cardViewModel.environment}")
+
+        val (monthString, yearString) =
+            expirationDate.split("/") ?: listOf("", "")
+
+        val card = Card(cardNumber, monthString, yearString)
+        card.securityCode = securityCode
+
+        lifecycleScope.launch {
+            val order = fetchOrder()
+            val request = CardRequest(order.id!!, card)
+
+            try {
+                val result = cardClient.approveOrder(request)
+                Log.d(TAG, "SUCCESS")
+                Log.d(TAG, "${result.status}")
+            } catch (error: PayPalSDKError) {
+                Log.e(TAG, "ERROR")
+                Log.e(TAG, error.errorDescription.orEmpty())
+            }
+        }
+    }
+
+    private suspend fun fetchOrder(): Order {
+        return payPalDemoApi.fetchOrderId(
+            countryCode = "CO",
+            orderRequest = CreateOrderRequest(
+                intent = "CAPTURE",
+                purchaseUnit = listOf(
+                    PurchaseUnit(
+                        amount = Amount(
+                            currencyCode = "USD",
+                            value = "10.99"
+                        )
+                    )
+                ),
+                payee = Payee(
+                    emailAddress = "anpelaez@paypal.com"
+                )
+            )
+        )
     }
 }
