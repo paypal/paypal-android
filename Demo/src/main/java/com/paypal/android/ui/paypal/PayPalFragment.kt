1d1a5a9d614535b51eeaf734f8888aeabc4af5d0
sshropshire <sshropshire@paypal.com>
2022-03-14 10:14:46 -0500

    Remove compose.

--- Demo/src/main/java/com/paypal/android/ui/paypal/PayPalFragment.kt
+++ Demo/src/main/java/com/paypal/android/ui/paypal/PayPalFragment.kt
@@ -5,27 +5,6 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Button
-import androidx.compose.material.CircularProgressIndicator
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.livedata.observeAsState
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.ComposeView
-import androidx.compose.ui.platform.ViewCompositionStrategy
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
@@ -42,7 +21,6 @@ import com.paypal.android.core.CoreConfig
 import com.paypal.android.core.Environment
 import com.paypal.android.core.PayPalSDKError
 import com.paypal.android.databinding.FragmentPaymentButtonBinding
-import com.paypal.android.ui.theme.DemoTheme
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
 import retrofit2.HttpException
@@ -80,71 +58,6 @@ class PayPalFragment : Fragment(), PayPalListener {
         binding.submitButton.setOnClickListener { launchNativeCheckout() }
 
         return binding.root
-    }
-
-    @Preview
-    @Composable
-    fun PayPalFragmentView() = DemoTheme {
-        val isLoading by payPalViewModel.isLoading.observeAsState(initial = false)
-        ConstraintLayout(
-            modifier = Modifier.fillMaxHeight(),
-        ) {
-            val (button, text, result) = createRefs()
-            if (isLoading) {
-                LoadingComposable(modifier = Modifier.constrainAs(result) {
-                    top.linkTo(parent.top)
-                    bottom.linkTo(button.top)
-                    start.linkTo(parent.start)
-                    end.linkTo(parent.end)
-                })
-            } else {
-                CheckoutResult(
-                    modifier = Modifier.constrainAs(result) {
-                        top.linkTo(parent.top)
-                        bottom.linkTo(button.top)
-                    })
-            }
-            Button(
-                enabled = !isLoading,
-                onClick = { launchNativeCheckout() },
-                modifier = Modifier
-                    .padding(start = 16.dp, end = 16.dp, bottom = 16.dp, top = 16.dp)
-                    .fillMaxWidth()
-                    .constrainAs(button) {
-                        bottom.linkTo(text.top)
-                    }
-            ) {
-                Text(stringResource(R.string.start_checkout))
-            }
-        }
-    }
-
-    @Composable
-    private fun CheckoutResult(modifier: Modifier) {
-        val statusTitle by payPalViewModel.statusTitle.observeAsState(initial = "")
-        val statusText by payPalViewModel.statusText.observeAsState(initial = "")
-        Column(modifier = modifier.padding(horizontal = 16.dp)) {
-            Text(
-                text = statusTitle,
-                fontSize = 24.sp,
-                fontWeight = FontWeight.Black,
-                modifier = Modifier.padding(bottom = 8.dp)
-            )
-            Text(
-                text = statusText,
-                fontSize = 16.sp,
-                color = Color.Gray,
-                modifier = Modifier.padding(bottom = 4.dp)
-            )
-        }
-    }
-
-    @Composable
-    private fun LoadingComposable(modifier: Modifier) {
-        Column(modifier = modifier, horizontalAlignment = Alignment.CenterHorizontally) {
-            CircularProgressIndicator(modifier = Modifier.padding(bottom = 8.dp))
-            Text(text = getString(R.string.creating_order))
-        }
     }
 
     override fun onPayPalSuccess(result: PayPalCheckoutResult) {
