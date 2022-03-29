b09acbb3f375025b0dc3111a988aaca4ca6b7641
Jose Noriega <jonoriega@paypal.com>
2022-03-29 18:31:18 -0300

    Added PayPalDataCollector

---
+++ PayPalDataCollector/src/main/java/com/paypal/android/paypaldatacollector/PayPalDataCollectorEnvironment.kt
@@ -0,0 +1,13 @@
+package com.paypal.android.paypaldatacollector
+
+enum class PayPalDataCollectorEnvironment {
+    LIVE,
+    SANDBOX,
+    STAGING
+}
+
+internal fun getMagnesEnvironment(environment: PayPalDataCollectorEnvironment) = when (environment) {
+    PayPalDataCollectorEnvironment.LIVE -> lib.android.paypal.com.magnessdk.Environment.LIVE
+    PayPalDataCollectorEnvironment.STAGING -> lib.android.paypal.com.magnessdk.Environment.STAGE
+    PayPalDataCollectorEnvironment.SANDBOX -> lib.android.paypal.com.magnessdk.Environment.SANDBOX
+}
