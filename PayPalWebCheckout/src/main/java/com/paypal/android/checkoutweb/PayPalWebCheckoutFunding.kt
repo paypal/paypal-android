4985c5e815433b03ecf70889f4ec875ed9ddf457
Jose Noriega <jonoriega@paypal.com>
2022-04-05 15:03:18 -0300

    Adds more doc strings

--- PayPalWebCheckout/src/main/java/com/paypal/android/checkoutweb/PayPalWebCheckoutFunding.kt
+++ PayPalWebCheckout/src/main/java/com/paypal/android/checkoutweb/PayPalWebCheckoutFunding.kt
@@ -1,5 +1,8 @@
 package com.paypal.android.checkoutweb
 
+/**
+ * Enum class to specify the type of funding for an order
+ */
 enum class PayPalWebCheckoutFunding(val value: String) {
     CREDIT("credit"), PAY_LATER("paylater"), DEFAULT("")
 }
