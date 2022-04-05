b53717ce4900831a30e3c79acb75d131e5c8e251
Jose Noriega <jonoriega@paypal.com>
2022-04-05 14:53:43 -0300

    Adds docs

--- PayPalWebCheckout/src/main/java/com/paypal/android/checkoutweb/PayPalWebCheckoutRequest.kt
+++ PayPalWebCheckout/src/main/java/com/paypal/android/checkoutweb/PayPalWebCheckoutRequest.kt
@@ -4,5 +4,6 @@ package com.paypal.android.checkoutweb
  * Creates an instance of a PayPalRequest.
  *
  * @param orderID The ID of the order to be approved.
+ * @param funding specify funding (credit, paylater or default)
  */
 data class PayPalWebCheckoutRequest @JvmOverloads constructor (val orderID: String, val funding: PayPalWebCheckoutFunding = PayPalWebCheckoutFunding.DEFAULT)
