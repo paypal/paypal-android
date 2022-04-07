a414db419fefaa49a97419fa84d81ec6f57f5a0d
Jose Noriega <jonoriega@paypal.com>
2022-04-07 13:28:33 -0300

    Doc strings udapted

--- PayPalWebCheckout/src/main/java/com/paypal/android/checkoutweb/PayPalWebCheckoutFundingSource.kt
+++ PayPalWebCheckout/src/main/java/com/paypal/android/checkoutweb/PayPalWebCheckoutFundingSource.kt
@@ -2,15 +2,18 @@ package com.paypal.android.checkoutweb
 
 /**
  * Enum class to specify the type of funding for an order.
+ * For more information go to: https://developer.paypal.com/docs/checkout/pay-later/us/
  */
 enum class PayPalWebCheckoutFundingSource(val value: String) {
     /**
      * PAYPAL_CREDIT will launch the web checkout flow and display PayPal Credit funding to eligible customers
+     * Eligible costumers receive a revolving line of credit that they can use to pay over time.
      */
     PAYPAL_CREDIT("credit"),
 
     /**
-     * PAY_LATER will launch the web checkout flow and display Pay Later offers to eligible customers.
+     * PAY_LATER will launch the web checkout flow and display Pay Later offers to eligible customers,
+     * which include short-term, interest-free payments and other special financing options.
      */
     PAY_LATER("paylater"),
 
