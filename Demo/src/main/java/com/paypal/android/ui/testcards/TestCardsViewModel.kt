2fe4a93c1621eddca8eabc72829618536f8e71e7
sshropshire <sshropshire@paypal.com>
2022-06-08 11:02:52 -0500

    Add remaining test cards.

--- Demo/src/main/java/com/paypal/android/ui/testcards/TestCardsViewModel.kt
+++ Demo/src/main/java/com/paypal/android/ui/testcards/TestCardsViewModel.kt
@@ -35,20 +35,110 @@ class TestCardsViewModel : ViewModel() {
             name = "Cards with 3DS",
             cards = listOf(
                 TestCard(
-                    name = "3DS1",
+                    name = "3DS Successful Auth",
                     card = Card(
                         number = "4000000000000002",
                         expirationMonth = "01",
-                        expirationYear = "2026",
+                        expirationYear = "2023",
                         securityCode = "123"
                     ),
                 ),
                 TestCard(
-                    name = "3DS2",
+                    name = "3DS Failed Signature",
                     card = Card(
-                        number = "4000000000001091",
+                        number = "4000000000000010",
                         expirationMonth = "01",
-                        expirationYear = "2026",
+                        expirationYear = "2023",
+                        securityCode = "123"
+                    ),
+                ),
+                TestCard(
+                    name = "3DS Failed Authentication",
+                    card = Card(
+                        number = "4000000000000028",
+                        expirationMonth = "01",
+                        expirationYear = "2023",
+                        securityCode = "123"
+                    ),
+                ),
+                TestCard(
+                    name = "3DS Passive Authentication",
+                    card = Card(
+                        number = "4000000000000101",
+                        expirationMonth = "01",
+                        expirationYear = "2023",
+                        securityCode = "123"
+                    ),
+                ),
+                TestCard(
+                    name = "3DS Transaction Timeout",
+                    card = Card(
+                        number = "4000000000000044",
+                        expirationMonth = "01",
+                        expirationYear = "2023",
+                        securityCode = "123"
+                    ),
+                ),
+                TestCard(
+                    name = "3DS Not Enrolled",
+                    card = Card(
+                        number = "4000000000000051",
+                        expirationMonth = "01",
+                        expirationYear = "2023",
+                        securityCode = "123"
+                    ),
+                ),
+                TestCard(
+                    name = "3DS Auth System Unavailable",
+                    card = Card(
+                        number = "4000000000000069",
+                        expirationMonth = "01",
+                        expirationYear = "2023",
+                        securityCode = "123"
+                    ),
+                ),
+                TestCard(
+                    name = "3DS Auth Error",
+                    card = Card(
+                        number = "4000000000000093",
+                        expirationMonth = "01",
+                        expirationYear = "2023",
+                        securityCode = "123"
+                    ),
+                ),
+                TestCard(
+                    name = "3DS Auth Unavailable",
+                    card = Card(
+                        number = "4000000000000036",
+                        expirationMonth = "01",
+                        expirationYear = "2023",
+                        securityCode = "123"
+                    ),
+                ),
+                TestCard(
+                    name = "3DS Merchant Bypassed Auth",
+                    card = Card(
+                        number = "4000990000000004",
+                        expirationMonth = "01",
+                        expirationYear = "2023",
+                        securityCode = "123"
+                    ),
+                ),
+                TestCard(
+                    name = "3DS Merchant Inactive",
+                    card = Card(
+                        number = "4000000000000077",
+                        expirationMonth = "01",
+                        expirationYear = "2023",
+                        securityCode = "123"
+                    ),
+                ),
+                TestCard(
+                    name = "3DS cmpi_lookup Error",
+                    card = Card(
+                        number = "4000000000000085",
+                        expirationMonth = "01",
+                        expirationYear = "2023",
                         securityCode = "123"
                     ),
                 ),
