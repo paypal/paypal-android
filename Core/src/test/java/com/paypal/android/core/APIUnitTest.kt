708e307105523034db85bb8b0b5a07740ba670b2
Steven Shropshire <steven.shropshire@getbraintree.com>
2022-07-26 12:03:25 -0500

    Add test for get client id in API module.
    Signed-off-by: Andres Pelaez <anpelaez@paypal.com>

--- Core/src/test/java/com/paypal/android/core/APIUnitTest.kt
+++ Core/src/test/java/com/paypal/android/core/APIUnitTest.kt
@@ -3,13 +3,17 @@ package com.paypal.android.core
 import io.mockk.coEvery
 import io.mockk.every
 import io.mockk.mockk
+import io.mockk.runs
+import io.mockk.slot
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.test.TestCoroutineDispatcher
 import kotlinx.coroutines.test.resetMain
 import kotlinx.coroutines.test.setMain
+import org.json.JSONObject
 import org.junit.After
+import org.junit.Assert.assertEquals
 import org.junit.Assert.assertSame
 import org.junit.Before
 import org.junit.Test
@@ -25,6 +29,13 @@ class APIUnitTest {
     private val configuration = CoreConfig()
 
     private val testCoroutineDispatcher = TestCoroutineDispatcher()
+
+    private val clientIdSuccessResponse by lazy {
+        val clientIdBody = JSONObject()
+            .put("client_id", "sample-client-id")
+            .toString()
+        HttpResponse(200, emptyMap(), clientIdBody)
+    }
 
     private lateinit var sut: API
 
@@ -51,11 +62,46 @@ class APIUnitTest {
         } returns httpRequest
 
         val httpResponse = HttpResponse(200)
-        coEvery {
-            http.send(httpRequest)
-        } returns httpResponse
+        coEvery { http.send(httpRequest) } returns httpResponse
 
         val result = sut.send(apiRequest)
         assertSame(httpResponse, result)
+    }
+
+    @Test
+    fun `get client id sends oauth api request`() = runBlocking {
+        val url = URL("https://example.com/resolved/path")
+        val httpRequest = HttpRequest(url, HttpMethod.GET)
+
+        val apiRequestSlot = slot<APIRequest>()
+        every {
+            httpRequestFactory.createHttpRequestFromAPIRequest(
+                capture(apiRequestSlot),
+                configuration
+            )
+        } returns httpRequest
+
+        coEvery { http.send(httpRequest) } returns clientIdSuccessResponse
+
+        sut.getClientId()
+
+        val apiRequest = apiRequestSlot.captured
+        assertEquals(HttpMethod.GET, apiRequest.method)
+        assertEquals("v1/oauth2/token", apiRequest.path)
+    }
+
+    @Test
+    fun `get client id returns client id from JSON`() = runBlocking {
+        val url = URL("https://example.com/resolved/path")
+        val httpRequest = HttpRequest(url, HttpMethod.GET)
+
+        every {
+            httpRequestFactory.createHttpRequestFromAPIRequest(any(), any())
+        } returns httpRequest
+
+        coEvery { http.send(httpRequest) } returns clientIdSuccessResponse
+
+        val result = sut.getClientId()
+        assertEquals("sample-client-id", result)
     }
 }
