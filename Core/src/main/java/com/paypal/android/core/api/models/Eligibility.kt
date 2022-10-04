4cc6fd3f5dbfcde7cc08db2e9655fa19aacb5bd7
Jose Noriega <jonoriega@paypal.com>
2022-10-04 15:32:56 -0300

    Add doc string to elgibility api and elgiblity class

--- Core/src/main/java/com/paypal/android/core/api/models/Eligibility.kt
+++ Core/src/main/java/com/paypal/android/core/api/models/Eligibility.kt
@@ -1,5 +1,8 @@
 package com.paypal.android.core.api.models
 
+/**
+ *  Class representing eligibility for a set of payment methods
+ */
 data class Eligibility(
     val isVenmoEligible: Boolean,
     val isPaypalEligible: Boolean,
