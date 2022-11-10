fefecdba3f3452135bdb64da3d459fedbfeeb111
Jose Noriega <jonoriega@paypal.com>
2022-11-10 12:46:04 -0300

    Overload 3d request constructor

--- Card/src/main/java/com/paypal/android/card/threedsecure/ThreeDSecureRequest.kt
+++ Card/src/main/java/com/paypal/android/card/threedsecure/ThreeDSecureRequest.kt
@@ -1,3 +1,7 @@
 package com.paypal.android.card.threedsecure
 
-data class ThreeDSecureRequest(val sca: SCA = SCA.SCA_WHEN_REQUIRED, val returnUrl: String, val cancelUrl: String)
+data class ThreeDSecureRequest @JvmOverloads constructor(
+    val sca: SCA = SCA.SCA_WHEN_REQUIRED,
+    val returnUrl: String,
+    val cancelUrl: String
+)
