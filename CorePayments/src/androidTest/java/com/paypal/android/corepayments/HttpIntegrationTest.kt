669ab1398533d249ae2630b204b1f4c70726842f
Sammy Cannillo <sammyjaynecannillo@gmail.com>
2023-02-21 10:50:37 -0600

    Revert accidental comment out of HttpIntegrationTest.kt

--- CorePayments/src/androidTest/java/com/paypal/android/corepayments/HttpIntegrationTest.kt
+++ CorePayments/src/androidTest/java/com/paypal/android/corepayments/HttpIntegrationTest.kt
@@ -1,37 +1,37 @@
-//package com.paypal.android.corepayments
-//
-//import androidx.test.ext.junit.runners.AndroidJUnit4
-//import kotlinx.coroutines.ExperimentalCoroutinesApi
-//import kotlinx.coroutines.test.TestCoroutineDispatcher
-//import kotlinx.coroutines.test.runBlockingTest
-//import org.junit.After
-//import org.junit.Assert.assertEquals
-//import org.junit.Before
-//import org.junit.Test
-//import org.junit.runner.RunWith
-//
-//@ExperimentalCoroutinesApi
-//@RunWith(AndroidJUnit4::class)
-//class HttpIntegrationTest {
-//
-//    private lateinit var testDispatcher: TestCoroutineDispatcher
-//
-//    @Before
-//    fun beforeEach() {
-//        testDispatcher = TestCoroutineDispatcher()
-//    }
-//
-//    @After
-//    fun afterEach() {
-//        testDispatcher.cleanupTestCoroutines()
-//    }
-//
-//    @Test
-//    fun send_makesAnHttpRequest() = runBlockingTest {
-//        val request = HttpRequest("https://www.google.com")
-//
-//        val sut = Http(testDispatcher)
-//        val result = sut.send(request)
-//        assertEquals(200, result.status)
-//    }
-//}
+package com.paypal.android.corepayments
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.TestCoroutineDispatcher
+import kotlinx.coroutines.test.runBlockingTest
+import org.junit.After
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@ExperimentalCoroutinesApi
+@RunWith(AndroidJUnit4::class)
+class HttpIntegrationTest {
+
+    private lateinit var testDispatcher: TestCoroutineDispatcher
+
+    @Before
+    fun beforeEach() {
+        testDispatcher = TestCoroutineDispatcher()
+    }
+
+    @After
+    fun afterEach() {
+        testDispatcher.cleanupTestCoroutines()
+    }
+
+    @Test
+    fun send_makesAnHttpRequest() = runBlockingTest {
+        val request = HttpRequest("https://www.google.com")
+
+        val sut = Http(testDispatcher)
+        val result = sut.send(request)
+        assertEquals(200, result.status)
+    }
+}
