5b1dfee829e52990fd08466314001e52476fa60f
sshropshire <sshropshire@paypal.com>
2023-04-06 16:13:30 -0500

    Write test for eligibility API.

--- CorePayments/src/main/java/com/paypal/android/corepayments/api/EligibilityAPI.kt
+++ CorePayments/src/main/java/com/paypal/android/corepayments/api/EligibilityAPI.kt
@@ -4,6 +4,8 @@ import android.content.Context
 import com.paypal.android.corepayments.API
 import com.paypal.android.corepayments.CoreConfig
 import com.paypal.android.corepayments.PayPalSDKError
+import com.paypal.android.corepayments.R
+import com.paypal.android.corepayments.ResourceLoader
 import com.paypal.android.corepayments.api.models.Eligibility
 import com.paypal.android.corepayments.graphql.common.GraphQLClient
 import com.paypal.android.corepayments.graphql.common.GraphQLClientImpl
@@ -11,6 +13,7 @@ import com.paypal.android.corepayments.graphql.fundingEligibility.FundingEligibi
 import com.paypal.android.corepayments.graphql.fundingEligibility.models.FundingEligibilityIntent
 import com.paypal.android.corepayments.graphql.fundingEligibility.models.SupportedCountryCurrencyType
 import com.paypal.android.corepayments.graphql.fundingEligibility.models.SupportedPaymentMethodsType
+import org.json.JSONArray
 import org.json.JSONObject
 
 /**
@@ -18,17 +21,16 @@ import org.json.JSONObject
  */
 internal class EligibilityAPI internal constructor(
     private val api: API,
-    private val graphQLClient: GraphQLClient
+    private val graphQLClient: GraphQLClient,
+    private val resourceLoader: ResourceLoader
 ) {
-
     /**
-     *  EligibilityAPI constructor
-     *  @param coreConfig configuration parameters for eligibility API
+     *  EligibilityAPI constructor.
+     *  @param context Android context
+     *  @param config configuration parameters for eligibility API
      */
-    constructor(coreConfig: CoreConfig, context: Context) : this(
-        API(coreConfig, context),
-        GraphQLClientImpl(coreConfig)
-    )
+    constructor(context: Context, config: CoreConfig) :
+            this(API(config, context), GraphQLClientImpl(config), ResourceLoader(context))
 
     /**
      *  Checks if merchant is eligible for a set of payment methods
@@ -36,7 +38,20 @@ internal class EligibilityAPI internal constructor(
      *  @throws PayPalSDKError if something went wrong in the API call
      */
     suspend fun checkEligibility(): Eligibility {
-        graphQLClient.send(JSONObject())
+        val clientID = api.fetchCachedOrRemoteClientID()
+
+        val query = resourceLoader.loadRawResource(R.raw.graphql_query_funding_eligibility)
+        val enableFundingMethods = listOf(SupportedPaymentMethodsType.VENMO.name)
+        val variables = JSONObject()
+            .put("clientId", clientID)
+            .put("intent", FundingEligibilityIntent.CAPTURE.name)
+            .put("currency", SupportedCountryCurrencyType.USD.name)
+            .put("enableFunding", JSONArray(enableFundingMethods))
+
+        val graphQLRequest = JSONObject()
+            .put("query", query)
+            .put("variables", variables)
+        val graphQLResponse = graphQLClient.send(graphQLRequest)
         return Eligibility(false, false, false, false, false)
 
         val fundingEligibilityQuery = FundingEligibilityQuery(
--- CorePayments/src/test/java/com/paypal/android/corepayments/api/EligibilityAPIUnitTest.kt
+++ CorePayments/src/test/java/com/paypal/android/corepayments/api/EligibilityAPIUnitTest.kt
@@ -6,16 +6,19 @@ import androidx.test.core.app.ApplicationProvider
 import com.paypal.android.corepayments.API
 import com.paypal.android.corepayments.ResourceLoader
 import com.paypal.android.corepayments.graphql.common.GraphQLClient
+import io.mockk.coEvery
 import io.mockk.coVerify
 import io.mockk.mockk
 import io.mockk.slot
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.runTest
+import org.json.JSONArray
 import org.json.JSONObject
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
+import org.skyscreamer.jsonassert.JSONAssert
 
 @ExperimentalCoroutinesApi
 @RunWith(RobolectricTestRunner::class)
@@ -38,18 +41,29 @@ class EligibilityAPIUnitTest {
 
     @Test
     fun checkEligibility_sendsGraphQLRequest() = runTest {
-        sut = EligibilityAPI(api, graphQLClient)
+        coEvery { api.fetchCachedOrRemoteClientID() } returns "sample-client-id"
 
+        sut = EligibilityAPI(api, graphQLClient, resourceLoader)
         sut.checkEligibility()
 
         val requestBodySlot = slot<JSONObject>()
         coVerify { graphQLClient.send(capture(requestBodySlot)) }
-        val requestBody = requestBodySlot.captured
+        val actualRequestBody = requestBodySlot.captured
 
         val expectedQuery = resourceLoader.loadRawResource(R.raw.graphql_query_funding_eligibility)
-        val expectedVariables = JSONObject()
-        val expectedJSON = JSONObject()
-            .put("query", expectedQuery)
-            .put("variables", expectedVariables)
+        // language=JSON
+        val expectedRequestBody = """
+        {
+            "query": "$expectedQuery",
+            "variables": {
+              "clientId": "sample-client-id",
+              "intent": "CAPTURE",
+              "currency": "USD",
+              "enableFunding": ["VENMO"]
+            }
+        }
+        """
+
+        JSONAssert.assertEquals(JSONObject(expectedRequestBody), actualRequestBody, true)
     }
 }
