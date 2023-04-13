988c6bc4b4a6b4386d74342f0a9c9659e6bae288
Jose Noriega <jonoriega@paypal.com>
2023-04-13 09:51:39 -0300

    Add new lines

--- PayPalNativePayments/src/main/java/com/paypal/android/paypalnativepayments/PayPalNativeShippingMethod.kt
+++ PayPalNativePayments/src/main/java/com/paypal/android/paypalnativepayments/PayPalNativeShippingMethod.kt
@@ -10,10 +10,12 @@ import com.paypal.checkout.order.Options
  * on your server. Otherwise, our Paysheet won't display any shipping options.
  */
 data class PayPalNativeShippingMethod internal constructor(
+
     /**
      * A unique ID that identifies a payer-selected shipping option.
      */
     val id: String,
+
     /**
      * If true it represents the shipping option that the merchant expects to be selected for the buyer
      * when they view the shipping options within the PayPal Checkout experience.
@@ -21,16 +23,19 @@ data class PayPalNativeShippingMethod internal constructor(
      * Only one shipping option per purchase unit can be selected.
      */
     val selected: Boolean,
+
     /**
      * A description that the payer sees, which helps them choose an appropriate shipping option.
      * For example, Free Shipping, USPS Priority Shipping, Expédition prioritaire USPS, or USPS yōuxiān fā huò.
      * Localize this description to the payer's locale.
      */
     val label: String,
+
     /**
      * The method by which the payer wants to get their items.
      */
     val type: PayPalNativeShippingType?,
+
     /**
      * The shipping cost for the selected option, which might be:
      * An integer for currencies like JPY that are not typically fractional.
@@ -39,6 +44,7 @@ data class PayPalNativeShippingMethod internal constructor(
      * Pattern: ^((-?[0-9]+)|(-?([0-9]+)?[.][0-9]+))$
      */
     val value: String?,
+
     /**
      * The [three-character ISO-4217 currency code](https://developer.paypal.com/docs/api/reference/currency-codes/)
      * that identifies the currency.
