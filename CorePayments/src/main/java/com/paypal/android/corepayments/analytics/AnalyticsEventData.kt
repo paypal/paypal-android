b6a9bf3c0d7ae522de155fedc85c707ac067e013
sshropshire <sshropshire@paypal.com>
2023-04-28 11:50:00 -0500

    Add TODO reference.

--- CorePayments/src/main/java/com/paypal/android/corepayments/analytics/AnalyticsEventData.kt
+++ CorePayments/src/main/java/com/paypal/android/corepayments/analytics/AnalyticsEventData.kt
@@ -1,5 +1,6 @@
 package com.paypal.android.corepayments.analytics
 
+// Ref: https://blog.klipse.tech/databook/2022/06/22/separate-code-from-data.html
 data class AnalyticsEventData(
     val clientID: String,
     val environment: String,
