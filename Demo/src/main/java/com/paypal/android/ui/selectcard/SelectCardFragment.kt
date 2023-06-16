131c689ae94a3479feb5869a2ea09c8323498c1a
sshropshire <sshropshire@paypal.com>
2023-06-16 15:19:22 -0500

    Update layout for select card fragment.

--- Demo/src/main/java/com/paypal/android/ui/selectcard/SelectCardFragment.kt
+++ Demo/src/main/java/com/paypal/android/ui/selectcard/SelectCardFragment.kt
@@ -4,20 +4,28 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.selection.selectable
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.platform.ViewCompositionStrategy
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.paypal.android.data.card.TestCard
 
 class SelectCardFragment : Fragment() {
 
@@ -30,28 +38,49 @@ class SelectCardFragment : Fragment() {
         setContent {
             MaterialTheme {
                 Surface(modifier = Modifier.fillMaxSize()) {
-                    SelectCardView()
+                    SelectCardView(onTestCardSelected = { onTestCardSelected(it) })
                 }
             }
         }
     }
 
+    private fun onTestCardSelected(card: TestCard) {
+        print(card)
+    }
+
     @OptIn(ExperimentalFoundationApi::class)
     @ExperimentalMaterial3Api
     @Composable
-    fun SelectCardView(viewModel: SelectCardViewModel = viewModel()) {
-        LazyColumn {
+    fun SelectCardView(
+        viewModel: SelectCardViewModel = viewModel(),
+        onTestCardSelected: (TestCard) -> Unit = {}
+    ) {
+        LazyColumn(
+            modifier = Modifier.fillMaxWidth()
+        ) {
             stickyHeader {
-                Text("Cards without 3DS")
+                TestCardHeader("Cards without 3DS")
             }
             items(viewModel.nonThreeDSCards) { card ->
-                Text(card.name)
+                TestCardView(
+                    card = card,
+                    selected = false,
+                    onClick = {
+                        onTestCardSelected(card)
+                    }
+                )
             }
             stickyHeader {
-                Text("Cards with 3DS")
+                TestCardHeader("Cards with 3DS")
             }
             items(viewModel.threeDSCards) { card ->
-                Text(card.name)
+                TestCardView(
+                    card = card,
+                    selected = false,
+                    onClick = {
+                        onTestCardSelected(card)
+                    }
+                )
             }
         }
     }
@@ -64,6 +93,38 @@ class SelectCardFragment : Fragment() {
             Surface(modifier = Modifier.fillMaxSize()) {
                 SelectCardView()
             }
+        }
+    }
+
+    @Composable
+    fun TestCardHeader(text: String) {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(Color.Black)
+        ) {
+            Text(
+                text = text,
+                color = Color.White,
+                modifier = Modifier.padding(8.dp)
+            )
+        }
+    }
+
+    @Composable
+    fun TestCardView(card: TestCard, selected: Boolean, onClick: () -> Unit) {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .selectable(
+                    selected = selected,
+                    onClick = onClick
+                )
+        ) {
+            Text(
+                text = card.name,
+                modifier = Modifier.padding(8.dp)
+            )
         }
     }
 }
