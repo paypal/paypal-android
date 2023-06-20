544d871c4c3d0d1e9dc98b9561aef2bd46cdfad3
sshropshire <sshropshire@paypal.com>
2023-06-20 00:43:16 -0500

    Update Expiration Date formatting code.

--- Demo/src/main/java/com/paypal/android/ui/card/ExpirationDateOffsetMapping.kt
+++ Demo/src/main/java/com/paypal/android/ui/card/ExpirationDateOffsetMapping.kt
@@ -2,12 +2,34 @@ package com.paypal.android.ui.card
 
 import androidx.compose.ui.text.input.OffsetMapping
 
-class ExpirationDateOffsetMapping : OffsetMapping {
+class ExpirationDateOffsetMapping(private val mode: Mode) : OffsetMapping {
+
+    enum class Mode {
+        DEFAULT, LEADING_ZERO
+    }
+
     override fun originalToTransformed(offset: Int): Int {
-        return offset
+        var transformed = offset
+        if (mode == Mode.LEADING_ZERO) {
+            transformed++
+        }
+
+        if (transformed >= 2) {
+            // increment for added slash in date
+            transformed++
+        }
+        return transformed
     }
 
     override fun transformedToOriginal(offset: Int): Int {
-        return offset
+        var transformed = offset
+        if (transformed >= 3) {
+            // decrement for added slash in date
+            transformed--
+        }
+        if (mode == Mode.LEADING_ZERO) {
+            transformed--
+        }
+        return transformed
     }
 }
--- Demo/src/main/java/com/paypal/android/ui/card/ExpirationDateVisualTransformation.kt
+++ Demo/src/main/java/com/paypal/android/ui/card/ExpirationDateVisualTransformation.kt
@@ -7,13 +7,54 @@ import androidx.compose.ui.text.input.VisualTransformation
 class ExpirationDateVisualTransformation : VisualTransformation {
 
     override fun filter(text: AnnotatedString): TransformedText {
+        var mode = ExpirationDateOffsetMapping.Mode.DEFAULT
+        if (text.isNotEmpty()) {
+            val firstChar = text[0]
+            if (firstChar != '0' && firstChar != '1') {
+                mode = ExpirationDateOffsetMapping.Mode.LEADING_ZERO
+            }
+        }
+
         return TransformedText(
             AnnotatedString(formatDate(text.text)),
-            offsetMapping = ExpirationDateOffsetMapping()
+            offsetMapping = ExpirationDateOffsetMapping(mode)
         )
     }
 
     private fun formatDate(rawDate: String): String {
-        return rawDate
+        var result = ""
+
+        val dateComponents = if (rawDate.length > 1) {
+            listOf(rawDate.substring(0, 2), rawDate.substring(2))
+        } else {
+            listOf(rawDate)
+        }
+
+        if(dateComponents.isNotEmpty()) {
+            val month = dateComponents[0]
+            result += if (month.length == 1) {
+                val firstMonthChar = month[0]
+                if (firstMonthChar == '0' || firstMonthChar == '1') {
+                    // passthrough
+                    month
+                } else {
+                    // prepend zero
+                    "0${month}"
+                }
+            } else {
+                // passthrough
+                month
+            }
+
+            if (result.length == 2) {
+                result += "/"
+            }
+
+            if (dateComponents.size > 1) {
+                // append remainder
+                result += dateComponents[1]
+            }
+        }
+        return result
     }
 }
