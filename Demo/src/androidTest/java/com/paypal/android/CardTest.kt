98d1e4b02303ad8257daf8e4a96105b9947e36ca
sshropshire <sshropshire@paypal.com>
2023-06-21 10:02:30 -0500

    Revert compileSdkVersion.

--- Demo/src/androidTest/java/com/paypal/android/CardTest.kt
+++ Demo/src/androidTest/java/com/paypal/android/CardTest.kt
@@ -5,9 +5,14 @@ import com.paypal.android.testutils.AppDriver
 import org.junit.Assert.assertTrue
 import org.junit.Test
 import org.junit.runner.RunWith
+import java.util.Calendar
 
 @RunWith(AndroidJUnit4::class)
 class CardTest {
+
+    companion object {
+        val validExpirationYear = Calendar.getInstance().get(Calendar.YEAR) + 3
+    }
 
     private val driver = AppDriver("com.paypal.android")
 
@@ -20,7 +25,7 @@ class CardTest {
             waitForText("Card Number")
 
             findText("Card Number").text = "4111111111111111"
-            findText("Expiration").text = "02/2023"
+            findText("Expiration").text = "01/$validExpirationYear"
             findText("Security Code").text = "123"
 
             findText("SUBMIT").click()
