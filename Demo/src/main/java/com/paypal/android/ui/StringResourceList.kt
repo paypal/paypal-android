39100428df1d6bed2e4eda827f92a5b3f7d0d4eb
sshropshire <sshropshire@paypal.com>
2023-06-21 11:07:26 -0500

    Cleanup code.

--- Demo/src/main/java/com/paypal/android/ui/StringResourceList.kt
+++ Demo/src/main/java/com/paypal/android/ui/StringResourceList.kt
@@ -5,6 +5,5 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.res.stringResource
 
 @Composable
-fun stringResourceListOf(@StringRes vararg resources: Int): List<String> {
-    return resources.map { stringResource(id = it) }
-}
+fun stringResourceListOf(@StringRes vararg resources: Int): List<String> =
+    resources.map { stringResource(id = it) }
