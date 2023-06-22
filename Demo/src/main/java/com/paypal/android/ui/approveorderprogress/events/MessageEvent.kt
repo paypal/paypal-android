2e95380b52ffc902b99f83544920980e645e7384
sshropshire <sshropshire@paypal.com>
2023-06-22 14:29:33 -0500

    Remove unused imports.

--- Demo/src/main/java/com/paypal/android/ui/approveorderprogress/events/MessageEvent.kt
+++ Demo/src/main/java/com/paypal/android/ui/approveorderprogress/events/MessageEvent.kt
@@ -4,11 +4,9 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedCard
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.paypal.android.ui.approveorderprogress.ComposableEvent
 
