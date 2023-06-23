35ac2b878e86766f5dcd4cc22ee15431d68da810
sshropshire <sshropshire@paypal.com>
2023-06-23 09:49:28 -0500

    Enable minification in Demo app and fix detekt error for too much nesting.

--- CardPayments/src/main/java/com/paypal/android/cardpayments/api/CheckoutOrdersRequestFactory.kt
+++ CardPayments/src/main/java/com/paypal/android/cardpayments/api/CheckoutOrdersRequestFactory.kt
@@ -7,62 +7,63 @@ import org.json.JSONObject
 
 internal class CheckoutOrdersRequestFactory {
 
-    fun createConfirmPaymentSourceRequest(cardRequest: CardRequest): APIRequest =
-        cardRequest.run {
-            val cardNumber = card.number.replace("\\s".toRegex(), "")
-            val cardExpiry = "${card.expirationYear}-${card.expirationMonth}"
+    fun createConfirmPaymentSourceRequest(cardRequest: CardRequest): APIRequest {
+        val card = cardRequest.card
+        val cardNumber = card.number.replace("\\s".toRegex(), "")
+        val cardExpiry = "${card.expirationYear}-${card.expirationMonth}"
 
-            val cardJSON = JSONObject()
-                .put("number", cardNumber)
-                .put("expiry", cardExpiry)
+        val cardJSON = JSONObject()
+            .put("number", cardNumber)
+            .put("expiry", cardExpiry)
 
-            card.cardholderName?.let { cardJSON.put("name", it) }
-            cardJSON.put("security_code", card.securityCode)
+        card.cardholderName?.let { cardJSON.put("name", it) }
+        cardJSON.put("security_code", card.securityCode)
 
-            card.billingAddress?.apply {
-                val billingAddressJSON = JSONObject()
-                    .put("address_line_1", streetAddress)
-                    .put("address_line_2", extendedAddress)
-                    .put("admin_area_1", region)
-                    .put("admin_area_2", locality)
-                    .put("postal_code", postalCode)
-                    .put("country_code", countryCode)
-                cardJSON.put("billing_address", billingAddressJSON)
-            }
+        card.billingAddress?.apply {
+            val billingAddressJSON = JSONObject()
+                .put("address_line_1", streetAddress)
+                .put("address_line_2", extendedAddress)
+                .put("admin_area_1", region)
+                .put("admin_area_2", locality)
+                .put("postal_code", postalCode)
+                .put("country_code", countryCode)
+            cardJSON.put("billing_address", billingAddressJSON)
+        }
 
-            val bodyJSON = JSONObject()
-            val verificationJSON = JSONObject()
-                .put("method", sca.name)
-            val attributesJSON = JSONObject()
-                .put("verification", verificationJSON)
+        val bodyJSON = JSONObject()
+        val verificationJSON = JSONObject()
+            .put("method", cardRequest.sca.name)
+        val attributesJSON = JSONObject()
+            .put("verification", verificationJSON)
 
-            if (cardRequest.shouldVault) {
-                val vaultJSON = JSONObject()
-                    .put("store_in_vault", "ON_SUCCESS")
-                attributesJSON.put("vault", vaultJSON)
+        if (cardRequest.shouldVault) {
+            val vaultJSON = JSONObject()
+                .put("store_in_vault", "ON_SUCCESS")
+            attributesJSON.put("vault", vaultJSON)
 
-                cardRequest.vaultCustomerId?.let { customerId ->
-                    if (customerId.isNotEmpty()) {
-                        val customerJSON = JSONObject()
-                            .put("id", customerId)
-                        attributesJSON.put("customer", customerJSON)
-                    }
+            cardRequest.vaultCustomerId?.let { customerId ->
+                if (customerId.isNotEmpty()) {
+                    val customerJSON = JSONObject()
+                        .put("id", customerId)
+                    attributesJSON.put("customer", customerJSON)
                 }
             }
-
-            cardJSON.put("attributes", attributesJSON)
-
-            val returnURLJSON = JSONObject()
-                .put("return_url", returnUrl)
-                .put("cancel_url", returnUrl) // we can set the same url
-            bodyJSON.put("application_context", returnURLJSON)
-
-            val paymentSourceJSON = JSONObject().put("card", cardJSON)
-            bodyJSON.put("payment_source", paymentSourceJSON)
-
-            val body = bodyJSON.toString().replace("\\/", "/")
-
-            val path = "v2/checkout/orders/$orderId/confirm-payment-source"
-            APIRequest(path, HttpMethod.POST, body)
         }
+
+        cardJSON.put("attributes", attributesJSON)
+
+        val returnUrl = cardRequest.returnUrl
+        val returnURLJSON = JSONObject()
+            .put("return_url", returnUrl)
+            .put("cancel_url", returnUrl) // we can set the same url
+        bodyJSON.put("application_context", returnURLJSON)
+
+        val paymentSourceJSON = JSONObject().put("card", cardJSON)
+        bodyJSON.put("payment_source", paymentSourceJSON)
+
+        val body = bodyJSON.toString().replace("\\/", "/")
+
+        val path = "v2/checkout/orders/${cardRequest.orderId}/confirm-payment-source"
+        return APIRequest(path, HttpMethod.POST, body)
+    }
 }
