5ed2d7a7689a0c0365563a12ba8e56fac93491b0
sshropshire <sshropshire@paypal.com>
2023-07-05 10:12:55 -0500

    Fix detekt lint errors.

--- Demo/src/main/java/com/paypal/android/ui/PaymentMethodsFragment.kt
+++ Demo/src/main/java/com/paypal/android/ui/PaymentMethodsFragment.kt
@@ -8,10 +8,8 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.ComposeView
