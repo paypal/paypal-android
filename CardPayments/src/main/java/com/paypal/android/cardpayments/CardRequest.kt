53c6fcba7bab2769d5cb3179012d2cd1c7f83e0c
sshropshire <sshropshire@paypal.com>
2023-07-13 10:50:31 -0500

    Add additional documentation on for CardRequest vaultCustomerId property.

--- CardPayments/src/main/java/com/paypal/android/cardpayments/CardRequest.kt
+++ CardPayments/src/main/java/com/paypal/android/cardpayments/CardRequest.kt
@@ -5,13 +5,16 @@ import com.paypal.android.cardpayments.threedsecure.SCA
 import kotlinx.parcelize.Parcelize
 
 /**
- * A card request to process a card payment
+ * A card request to process a card payment.
+ *
  * @property orderId The order ID to to process the payment
  * @property card   Card used for payment
  * @property returnUrl Url to return to app after SCA challenge finishes
  * @property sca Specify to always launch 3DS or only when required. Defaults to `SCA.SCA_WHEN_REQUIRED`.
  * @property shouldVault Set this value to true if the payment method should be vaulted on success
- * @property vaultCustomerId Set a customer id for vaulting payment methods when [shouldVault] is true
+ * @property vaultCustomerId Optional. When [shouldVault] is set to true and this property is set, the
+ * value of this property will be used to associate the vaulting of a payment method with a specific
+ * customer. If [shouldVault] is set to true and this property is not set, a vault customer ID will be generated.
  */
 @Parcelize
 data class CardRequest @JvmOverloads constructor(
