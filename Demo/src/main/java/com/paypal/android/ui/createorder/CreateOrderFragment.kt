144809d1c39904bbd46040677d05303fd242c327
sshropshire <sshropshire@paypal.com>
2023-07-19 15:31:26 -0500

    Add intent drop down to Order creation screen.

--- Demo/src/main/java/com/paypal/android/ui/createorder/CreateOrderFragment.kt
+++ Demo/src/main/java/com/paypal/android/ui/createorder/CreateOrderFragment.kt
@@ -3,23 +3,50 @@ package com.paypal.android.ui.createorder
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.platform.ViewCompositionStrategy
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.navArgs
+import com.paypal.android.R
+import com.paypal.android.api.model.Amount
+import com.paypal.android.api.model.CreateOrderRequest
+import com.paypal.android.api.model.Payee
+import com.paypal.android.api.model.PurchaseUnit
+import com.paypal.android.api.services.SDKSampleServerAPI
+import com.paypal.android.cardpayments.OrderIntent
+import com.paypal.android.ui.WireframeButton
+import com.paypal.android.ui.WireframeOptionDropDown
 import com.paypal.android.ui.features.Feature
+import com.paypal.android.ui.stringResourceListOf
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class CreateOrderFragment : Fragment() {
 
+    @Inject
+    lateinit var sdkSampleServerAPI: SDKSampleServerAPI
+
     private val args: CreateOrderFragmentArgs by navArgs()
+    private val viewModel by viewModels<CreateOrderViewModel>()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -28,19 +55,82 @@ class CreateOrderFragment : Fragment() {
     ) = ComposeView(requireContext()).apply {
         setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
         setContent {
+            val uiState by viewModel.uiState.collectAsStateWithLifecycle()
             MaterialTheme {
                 Surface(modifier = Modifier.fillMaxSize()) {
-                    CreateOrderView(feature = args.feature)
+                    CreateOrderView(
+                        feature = args.feature,
+                        uiState = uiState,
+                        onCreateOrderClick = {
+                            createOrder()
+                        }
+                    )
                 }
             }
         }
     }
 
+    private fun createOrder() {
+        viewLifecycleOwner.lifecycleScope.launch {
+            val uiState = viewModel.uiState.value
+            val orderIntent = when (uiState.intentOption) {
+                "AUTHORIZE" -> OrderIntent.AUTHORIZE
+                else -> OrderIntent.CAPTURE
+            }
+
+            val orderRequest = CreateOrderRequest(
+                intent = orderIntent.name,
+                purchaseUnit = listOf(
+                    PurchaseUnit(
+                        amount = Amount(
+                            currencyCode = "USD",
+                            value = "10.99"
+                        )
+                    )
+                ),
+                payee = Payee(emailAddress = "anpelaez@paypal.com")
+            )
+
+            val order = sdkSampleServerAPI.createOrder(orderRequest = orderRequest)
+        }
+    }
+
     @Composable
+    @OptIn(ExperimentalMaterial3Api::class)
     fun CreateOrderView(
-        feature: Feature
+        feature: Feature,
+        uiState: CreateOrderUiState,
+        onCreateOrderClick: () -> Unit
     ) {
-        Text("Create Order for ${stringResource(feature.stringRes)}")
+        Column(
+            modifier = Modifier
+                .padding(8.dp)
+        ) {
+            Text(text = "Create Order for ${stringResource(feature.stringRes)}")
+            WireframeOptionDropDown(
+                hint = stringResource(id = R.string.intent_title),
+                value = uiState.intentOption,
+                expanded = uiState.intentOptionExpanded,
+                options = stringResourceListOf(R.string.intent_authorize, R.string.intent_capture),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 8.dp),
+                onExpandedChange = { expanded ->
+                    viewModel.intentOptionExpanded = !expanded
+                },
+                onValueChange = { value ->
+                    viewModel.intentOption = value
+                    viewModel.intentOptionExpanded = false
+                }
+            )
+            WireframeButton(
+                text = "Create Order & Continue",
+                onClick = onCreateOrderClick,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 8.dp)
+            )
+        }
     }
 
     @Preview
@@ -48,7 +138,11 @@ class CreateOrderFragment : Fragment() {
     fun CreateOrderViewPreview() {
         MaterialTheme {
             Surface(modifier = Modifier.fillMaxSize()) {
-                CreateOrderView(Feature.CARD_APPROVE_ORDER)
+                CreateOrderView(
+                    feature = Feature.CARD_APPROVE_ORDER,
+                    uiState = CreateOrderUiState(),
+                    onCreateOrderClick = {}
+                )
             }
         }
     }
---
+++ Demo/src/main/java/com/paypal/android/ui/createorder/CreateOrderViewModel.kt
@@ -0,0 +1,23 @@
+package com.paypal.android.ui.createorder
+
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+
+class CreateOrderViewModel : ViewModel() {
+    private val _uiState = MutableStateFlow(CreateOrderUiState())
+    val uiState = _uiState.asStateFlow()
+
+    var intentOption: String
+        get() = _uiState.value.intentOption
+        set(value) {
+            _uiState.update { it.copy(intentOption = value) }
+        }
+
+    var intentOptionExpanded: Boolean
+        get() = _uiState.value.intentOptionExpanded
+        set(value) {
+            _uiState.update { it.copy(intentOptionExpanded = value) }
+        }
+}
