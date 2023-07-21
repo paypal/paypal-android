93c71dad50cd7a122cb6bcdb54de6ef94ea781cf
sshropshire <sshropshire@paypal.com>
2023-07-21 11:33:02 -0500

    Make VaultRequest Parcelable.

--- CardPayments/src/main/java/com/paypal/android/cardpayments/VaultRequest.kt
+++ CardPayments/src/main/java/com/paypal/android/cardpayments/VaultRequest.kt
@@ -1,12 +1,16 @@
 package com.paypal.android.cardpayments
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
 /**
  * @property card   Card used for payment
  * @property returnUrl Url to return to app after SCA challenge finishes
  * @property customerId Optional customer Id to associate when vaulting
  */
+@Parcelize
 data class VaultRequest(
     val card: Card,
     val returnUrl: String,
     val customerId: String? = null,
-)
+): Parcelable
