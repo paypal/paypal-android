1f5b2bd68d9264baae1d73f28fda4d5e5818df07
sshropshire <sshropshire@paypal.com>
2023-07-24 16:23:11 -0500

    Remove local integration type.

--- Demo/src/main/java/com/paypal/android/api/services/MerchantIntegration.kt
+++ Demo/src/main/java/com/paypal/android/api/services/MerchantIntegration.kt
@@ -1,7 +1,6 @@
 package com.paypal.android.api.services
 
 enum class MerchantIntegration(val baseUrl: String) {
-    LOCAL("http://10.0.2.2:8080/"),
     DEFAULT("https://sdk-sample-merchant-server.herokuapp.com/"),
     DIRECT("https://sdk-sample-merchant-server.herokuapp.com/direct/"),
     CONNECTED_PATH("https://sdk-sample-merchant-server.herokuapp.com/connected_path/"),
