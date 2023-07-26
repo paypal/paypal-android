5d579a745b84550bd467f467bd2b611a2b1202fb
sshropshire <sshropshire@paypal.com>
2023-07-26 09:01:22 -0500

    Add documentation for vault listener.

--- CardPayments/src/main/java/com/paypal/android/cardpayments/VaultListener.kt
+++ CardPayments/src/main/java/com/paypal/android/cardpayments/VaultListener.kt
@@ -2,8 +2,14 @@ package com.paypal.android.cardpayments
 
 import androidx.annotation.MainThread
 
+/**
+ * Listener to receive vault results.
+ */
 interface VaultListener {
 
+    /**
+     * Called after a successful vault occurs.
+     */
     @MainThread
     fun onVaultSuccess(result: VaultResult)
 }
--- CardPayments/src/main/java/com/paypal/android/cardpayments/VaultResult.kt
+++ CardPayments/src/main/java/com/paypal/android/cardpayments/VaultResult.kt
@@ -1,3 +1,13 @@
 package com.paypal.android.cardpayments
 
-data class VaultResult(val status: String, val setupTokenId: String)
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+/**
+ * Represents the result of a vault operation.
+ *
+ * @property status Vault status
+ * @property setupTokenId Associated setup token
+ */
+@Parcelize
+data class VaultResult(val status: String, val setupTokenId: String) : Parcelable
