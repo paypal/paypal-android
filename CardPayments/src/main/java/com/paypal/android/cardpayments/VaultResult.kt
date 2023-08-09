306f2c2004032c83e00637a4d40d4dc3629bf346
sshropshire <sshropshire@paypal.com>
2023-08-09 10:44:21 -0500

    Add docs for VaultResult type.

--- CardPayments/src/main/java/com/paypal/android/cardpayments/VaultResult.kt
+++ CardPayments/src/main/java/com/paypal/android/cardpayments/VaultResult.kt
@@ -3,6 +3,12 @@ package com.paypal.android.cardpayments
 import android.os.Parcelable
 import kotlinx.parcelize.Parcelize
 
+/**
+ * A result returned by [CardClient] when an a successful vault occurs.
+ *
+ * @param setupTokenId the id for the setup token that was recently updated
+ * @param status the status of the updated setup token
+ */
 @Parcelize
 data class VaultResult(
     val setupTokenId: String,
