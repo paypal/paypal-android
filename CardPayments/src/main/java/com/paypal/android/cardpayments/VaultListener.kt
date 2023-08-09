adeb34bfdf1a5ac83a4d06204a4f720534cc3af9
sshropshire <sshropshire@paypal.com>
2023-08-09 10:37:29 -0500

    Add docs to VaultListener.

--- CardPayments/src/main/java/com/paypal/android/cardpayments/VaultListener.kt
+++ CardPayments/src/main/java/com/paypal/android/cardpayments/VaultListener.kt
@@ -3,11 +3,20 @@ package com.paypal.android.cardpayments
 import androidx.annotation.MainThread
 import com.paypal.android.corepayments.PayPalSDKError
 
+/**
+ * Listener to receive callbacks form [CardClient.vault].
+ */
 interface VaultListener {
 
+    /**
+     * Called when a successful vault has occurred.
+     */
     @MainThread
     fun onVaultSuccess(result: VaultResult)
 
+    /**
+     * Called when a vault failure has occurred.
+     */
     @MainThread
     fun onVaultFailure(error: PayPalSDKError)
 }
