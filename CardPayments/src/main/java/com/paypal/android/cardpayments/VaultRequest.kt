4706f2cca26365a5034d8b055ba0edbe38d18576
sshropshire <sshropshire@paypal.com>
2023-08-09 10:38:42 -0500

    Add docs for VaultRequest.

--- CardPayments/src/main/java/com/paypal/android/cardpayments/VaultRequest.kt
+++ CardPayments/src/main/java/com/paypal/android/cardpayments/VaultRequest.kt
@@ -3,6 +3,12 @@ package com.paypal.android.cardpayments
 import android.os.Parcelable
 import kotlinx.parcelize.Parcelize
 
+/**
+ * A vault request to attach a payment method to a setup token.
+ *
+ * @property setupTokenId id for the setup token to update.
+ * @property card card payment source to attach to the setup token.
+ */
 @Parcelize
 data class VaultRequest(
     val setupTokenId: String,
