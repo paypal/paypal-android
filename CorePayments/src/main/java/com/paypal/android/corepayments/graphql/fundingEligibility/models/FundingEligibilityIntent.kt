dc121e38cda87f3b495d02d414539484951de3c7
sshropshire <sshropshire@paypal.com>
2023-09-12 13:15:34 -0500

    Remove Eligibility API files that are no longer used.

--- CorePayments/src/main/java/com/paypal/android/corepayments/graphql/fundingEligibility/models/FundingEligibilityIntent.kt
+++
@@ -1,5 +0,0 @@
-package com.paypal.android.corepayments.graphql.fundingEligibility.models
-
-internal enum class FundingEligibilityIntent {
-    SALE, CAPTURE, AUTHORIZE, ORDER, TOKENIZE, SUBSCRIPTION
-}
