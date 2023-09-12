ab7548594764b60d8cb2ab85e57f297542de95d1
sshropshire <sshropshire@paypal.com>
2023-09-12 11:20:31 -0500

    Remove OrderRequest class that is no longer used.

--- CardPayments/src/main/java/com/paypal/android/cardpayments/OrderRequest.kt
+++
@@ -1,5 +0,0 @@
-package com.paypal.android.cardpayments
-
-import com.paypal.android.cardpayments.model.PurchaseUnit
-
-data class OrderRequest(val intent: OrderIntent, val purchaseUnits: List<PurchaseUnit>?)
