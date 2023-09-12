5aa08a0fa5c85dd3c35600cda1a2781ec1914d9a
sshropshire <sshropshire@paypal.com>
2023-09-12 12:36:42 -0500

    Update PayPalWeb restrictions.

--- PayPalWebPayments/src/main/java/com/paypal/android/paypalwebpayments/PayPalDeepLinkUrlResult.kt
+++ PayPalWebPayments/src/main/java/com/paypal/android/paypalwebpayments/PayPalDeepLinkUrlResult.kt
@@ -4,7 +4,10 @@ import android.net.Uri
 import androidx.annotation.NonNull
 import org.json.JSONObject
 
-class PayPalDeepLinkUrlResult internal constructor(@NonNull uri: Uri, @NonNull metadata: JSONObject) {
+internal class PayPalDeepLinkUrlResult constructor(
+    @NonNull uri: Uri,
+    @NonNull metadata: JSONObject
+) {
     val payerId: String? = uri.getQueryParameter("PayerID")
     val intent: String? = uri.getQueryParameter("intent")
     val opType: String? = uri.getQueryParameter("opType")
