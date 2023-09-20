cda9ac0770e126534acc254fbc18024a289f1e7e
sshropshire <sshropshire@paypal.com>
2023-09-20 11:48:00 -0500

    Remove classes that are no longer needed.

--- Demo/src/main/java/com/paypal/android/api/model/Amount.kt
+++
@@ -1,9 +0,0 @@
-package com.paypal.android.api.model
-
-import com.google.gson.annotations.SerializedName
-
-data class Amount(
-    @SerializedName("currency_code")
-    val currencyCode: String?,
-    val value: String?
-)
--- Demo/src/main/java/com/paypal/android/api/model/Payee.kt
+++
@@ -1,8 +0,0 @@
-package com.paypal.android.api.model
-
-import com.google.gson.annotations.SerializedName
-
-data class Payee(
-    @SerializedName("email_address")
-    val emailAddress: String?
-)
