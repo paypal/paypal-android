af88b81f2fa45ed5b36588d6cc2c5f0f991b5a3f
Kevin Lay <kelay@paypal.com>
2023-09-25 14:39:19 -0700

    -Adding in auth config and tests

--- PayPalNativePayments/src/main/java/com/paypal/android/paypalnativepayments/PayPalNativeCheckoutRequest.kt
+++ PayPalNativePayments/src/main/java/com/paypal/android/paypalnativepayments/PayPalNativeCheckoutRequest.kt
@@ -7,5 +7,20 @@ data class PayPalNativeCheckoutRequest(
     /**
      * The order ID associated with the request.
      */
-    val orderId: String
+    val orderId: String,
+
+    /**
+     * Optional: The setup configuration for authentication
+     */
+    val authConfig: AuthConfig? = null
+)
+
+/**
+ * Used for setting up authentication configurations
+ */
+data class AuthConfig(
+    /**
+     * The email for preloading authentication
+     */
+    val userEmail: String? = null
 )
