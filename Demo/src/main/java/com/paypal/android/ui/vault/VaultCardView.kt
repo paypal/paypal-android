0b9b539d388c0f34a06e0f41383e223a32b5254f
sshropshire <sshropshire@paypal.com>
2023-11-14 11:28:57 -0600

    Extract VaultCardView from VaultFragment.

---
+++ Demo/src/main/java/com/paypal/android/ui/vault/VaultCardView.kt
@@ -0,0 +1,114 @@
+package com.paypal.android.ui.vault
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedCard
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.paypal.android.cardpayments.CardVaultResult
+import com.paypal.android.ui.approveorder.getActivity
+import com.paypal.android.uishared.components.PaymentTokenView
+import com.paypal.android.uishared.components.PropertyView
+import com.paypal.android.uishared.components.SetupTokenView
+
+@ExperimentalMaterial3Api
+@Composable
+fun VaultCardView(
+    viewModel: VaultViewModel,
+    onUseTestCardClick: () -> Unit,
+) {
+    val context = LocalContext.current
+    val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+    val scrollState = rememberScrollState()
+    LaunchedEffect(uiState) {
+        // continuously scroll to bottom of the list when event state is updated
+        scrollState.animateScrollTo(scrollState.maxValue)
+    }
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp)
+            .verticalScroll(scrollState)
+    ) {
+        CreateSetupTokenForm(
+            uiState = uiState,
+            onCustomerIdValueChange = { value -> viewModel.customerId = value },
+            onSubmit = { viewModel.createSetupToken() }
+        )
+        uiState.setupToken?.let { setupToken ->
+            Spacer(modifier = Modifier.size(8.dp))
+            SetupTokenView(setupToken = setupToken)
+            Spacer(modifier = Modifier.size(8.dp))
+            AttachCardToSetupTokenForm(
+                uiState = uiState,
+                onCardNumberChange = { viewModel.cardNumber = it },
+                onExpirationDateChange = { viewModel.cardExpirationDate = it },
+                onSecurityCodeChange = { viewModel.cardSecurityCode = it },
+                onUseTestCardClick = { onUseTestCardClick() },
+                onSubmit = { context.getActivity()?.let { viewModel.updateSetupToken(it) } }
+            )
+        }
+        uiState.cardVaultResult?.let { vaultResult ->
+            Spacer(modifier = Modifier.size(8.dp))
+            VaultSuccessView(cardVaultResult = vaultResult)
+            Spacer(modifier = Modifier.size(8.dp))
+            CreatePaymentTokenForm(
+                uiState = uiState,
+                onSubmit = { viewModel.createPaymentToken() }
+            )
+        }
+        uiState.paymentToken?.let { paymentToken ->
+            Spacer(modifier = Modifier.size(8.dp))
+            PaymentTokenView(paymentToken = paymentToken)
+        }
+    }
+}
+
+@Composable
+fun VaultSuccessView(cardVaultResult: CardVaultResult) {
+    OutlinedCard(
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        Column(
+            modifier = Modifier.padding(8.dp)
+        ) {
+            Text(
+                text = "Vault Success",
+                style = MaterialTheme.typography.titleLarge,
+            )
+            PropertyView(name = "Setup Token Id", value = cardVaultResult.setupTokenId)
+            PropertyView(name = "Status", value = cardVaultResult.status)
+        }
+    }
+}
+
+@ExperimentalMaterial3Api
+@Preview
+@Composable
+fun VaultCardViewPreview() {
+    MaterialTheme {
+        Surface(modifier = Modifier.fillMaxSize()) {
+            VaultCardView(
+                viewModel = viewModel(),
+                onUseTestCardClick = {}
+            )
+        }
+    }
+}
--- Demo/src/main/java/com/paypal/android/ui/vault/VaultViewModel.kt
+++ Demo/src/main/java/com/paypal/android/ui/vault/VaultViewModel.kt
@@ -1,15 +1,38 @@
 package com.paypal.android.ui.vault
 
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.paypal.android.api.model.PaymentToken
 import com.paypal.android.api.model.SetupToken
+import com.paypal.android.api.services.SDKSampleServerAPI
+import com.paypal.android.cardpayments.Card
+import com.paypal.android.cardpayments.CardClient
+import com.paypal.android.cardpayments.CardVaultListener
+import com.paypal.android.cardpayments.CardVaultRequest
 import com.paypal.android.cardpayments.CardVaultResult
+import com.paypal.android.corepayments.CoreConfig
+import com.paypal.android.corepayments.PayPalSDKError
 import com.paypal.android.models.TestCard
+import com.paypal.android.ui.approveorder.DateString
+import com.paypal.android.usecase.CreatePaymentTokenUseCase
+import com.paypal.android.usecase.CreateSetupTokenUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class VaultViewModel : ViewModel() {
+@HiltViewModel
+class VaultViewModel @Inject constructor(
+    val sdkSampleServerAPI: SDKSampleServerAPI,
+    val createSetupTokenUseCase: CreateSetupTokenUseCase,
+    val createPaymentTokenUseCase: CreatePaymentTokenUseCase
+) : ViewModel() {
+
+    private lateinit var cardClient: CardClient
+
     private val _uiState = MutableStateFlow(VaultUiState())
     val uiState = _uiState.asStateFlow()
 
@@ -82,5 +105,57 @@ class VaultViewModel : ViewModel() {
                 cardSecurityCode = card.securityCode
             )
         }
+    }
+
+    fun createSetupToken() {
+        viewModelScope.launch {
+            isCreateSetupTokenLoading = true
+            setupToken = createSetupTokenUseCase(customerId)
+            isCreateSetupTokenLoading = false
+        }
+    }
+
+    fun updateSetupToken(activity: AppCompatActivity) {
+        viewModelScope.launch {
+            isUpdateSetupTokenLoading = true
+            val clientId = sdkSampleServerAPI.fetchClientId()
+
+            val configuration = CoreConfig(clientId = clientId)
+            cardClient = CardClient(activity, configuration)
+            cardClient.cardVaultListener = object : CardVaultListener {
+                override fun onVaultSuccess(result: CardVaultResult) {
+                    isUpdateSetupTokenLoading = false
+                    cardVaultResult = result
+                }
+
+                override fun onVaultFailure(error: PayPalSDKError) {
+                    isUpdateSetupTokenLoading = false
+                    // TODO: handle error
+                }
+            }
+
+            val card = parseCard(_uiState.value)
+            val cardVaultRequest = CardVaultRequest(setupToken!!.id, card)
+            cardClient.vault(activity, cardVaultRequest)
+        }
+    }
+
+    fun createPaymentToken() {
+        viewModelScope.launch {
+            isCreatePaymentTokenLoading = true
+            paymentToken = createPaymentTokenUseCase(setupToken!!)
+            isCreatePaymentTokenLoading = false
+        }
+    }
+
+    private fun parseCard(uiState: VaultUiState): Card {
+        // expiration date in UI State needs to be formatted because it uses a visual transformation
+        val dateString = DateString(uiState.cardExpirationDate)
+        return Card(
+            number = uiState.cardNumber,
+            expirationMonth = dateString.formattedMonth,
+            expirationYear = dateString.formattedYear,
+            securityCode = uiState.cardSecurityCode
+        )
     }
 }
