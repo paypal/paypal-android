8893c88eb2cb2cf87ebde3eb40addee4453f8faf
sshropshire <sshropshire@paypal.com>
2023-11-20 16:03:35 -0600

    Add compose navigation view.

--- Demo/src/main/java/com/paypal/android/ui/DemoApp.kt
+++ Demo/src/main/java/com/paypal/android/ui/DemoApp.kt
@@ -1,8 +1,43 @@
 package com.paypal.android.ui
 
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.paypal.android.ui.approveorder.ApproveOrderView
+import com.paypal.android.ui.features.FeaturesView
+import com.paypal.android.ui.selectcard.SelectCardView
 
+@ExperimentalMaterial3Api
+@ExperimentalFoundationApi
 @Composable
 fun DemoApp() {
+    val navController = rememberNavController()
+    MaterialTheme {
+        Surface(modifier = Modifier.fillMaxSize()) {
+            NavHost(navController = navController, startDestination = "features") {
+                composable(DemoAppDestinations.FEATURES_ROUTE) {
+                    FeaturesView(onFeatureSelected = { feature ->
+                        navController.navigate(feature.routeName)
+                    })
+                }
+                composable(DemoAppDestinations.CARD_APPROVE_ORDER) {
+                    ApproveOrderView(onUseTestCardClick = {
+                        navController.navigate(DemoAppDestinations.SELECT_TEST_CARD)
+                    })
+                }
+                composable(DemoAppDestinations.SELECT_TEST_CARD) {
+                    SelectCardView(onTestCardSelected = { testCard ->
 
+                    })
+                }
+            }
+        }
+    }
 }
---
+++ Demo/src/main/java/com/paypal/android/ui/DemoAppDestinations.kt
@@ -0,0 +1,11 @@
+package com.paypal.android.ui
+
+object DemoAppDestinations {
+    const val CARD_APPROVE_ORDER = "card_approve_order"
+    const val FEATURES_ROUTE = "features"
+    const val CARD_VAULT = "card_vault"
+    const val PAYPAL_WEB = "paypal_web"
+    const val PAYPAL_BUTTONS = "paypal_buttons"
+    const val PAYPAL_NATIVE = "paypal_native"
+    const val SELECT_TEST_CARD = "select_test_card"
+}
