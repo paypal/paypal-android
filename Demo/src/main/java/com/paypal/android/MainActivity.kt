90b36e57f27a23b23851e8d8a928399b61c065c6
sshropshire <sshropshire@paypal.com>
2023-11-20 15:42:52 -0600

    Remove Jetpack Navigation configuration from MainActivity.

--- Demo/src/main/java/com/paypal/android/MainActivity.kt
+++ Demo/src/main/java/com/paypal/android/MainActivity.kt
@@ -1,48 +1,18 @@
 package com.paypal.android
 
-import android.content.Intent
 import android.os.Bundle
-import android.util.Log
+import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.NavController
-import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.NavigationUI
-import androidx.preference.PreferenceManager
+import com.paypal.android.ui.DemoApp
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var navHostFragment: NavHostFragment
-    private lateinit var navController: NavController
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_demo)
-
-        navHostFragment =
-            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        navController = navHostFragment.navController
-
-        val appBarConfiguration = AppBarConfiguration(navController.graph)
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration)
-
-        PreferenceManager.setDefaultValues(this, R.xml.preferences, false)
-    }
-
-    override fun onSupportNavigateUp(): Boolean {
-        return navController.navigateUp() || super.onSupportNavigateUp()
-    }
-
-    override fun onNewIntent(newIntent: Intent?) {
-        super.onNewIntent(newIntent)
-        intent = newIntent
-        Log.e("DEMO ACTIVITY", "ON NEW INTENT")
-    }
-
-    override fun onResume() {
-        super.onResume()
-        Log.e("DEMO ACTIVITY", "ON RESUME")
+        setContent {
+            DemoApp()
+        }
     }
 }
---
+++ Demo/src/main/java/com/paypal/android/ui/DemoApp.kt
@@ -0,0 +1,8 @@
+package com.paypal.android.ui
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun DemoApp() {
+
+}
