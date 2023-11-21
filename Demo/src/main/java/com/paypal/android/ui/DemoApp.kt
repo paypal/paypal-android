e29e0645500fee1ea311bcc48bc4d6a2b85e3f68
sshropshire <sshropshire@paypal.com>
2023-11-21 11:27:53 -0600

    Add Card Vault destination to project.

--- Demo/src/main/java/com/paypal/android/ui/DemoApp.kt
+++ Demo/src/main/java/com/paypal/android/ui/DemoApp.kt
@@ -8,7 +8,6 @@ import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.navigation.NavOptions
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -17,7 +16,10 @@ import com.paypal.android.ui.approveorder.ApproveOrderView
 import com.paypal.android.ui.approveorder.ApproveOrderViewModel
 import com.paypal.android.ui.features.FeaturesView
 import com.paypal.android.ui.selectcard.SelectCardView
+import com.paypal.android.ui.vaultcard.VaultCardView
+import com.paypal.android.ui.vaultcard.VaultCardViewModel
 
+// Ref: https://youtu.be/goFpG25uoc8?si=hqYGEaA95We6qUiE&t=76
 @ExperimentalMaterial3Api
 @ExperimentalFoundationApi
 @Composable
@@ -32,7 +34,6 @@ fun DemoApp() {
                     })
                 }
                 composable(DemoAppDestinations.CARD_APPROVE_ORDER) { entry ->
-                    // Ref: https://youtu.be/goFpG25uoc8?si=hqYGEaA95We6qUiE&t=76
                     val viewModel: ApproveOrderViewModel = hiltViewModel()
 
                     // prefill test card (if necessary)
@@ -43,9 +44,24 @@ fun DemoApp() {
                     }
 
                     // Ref: https://youtu.be/NhoV78E6yWo?si=zZR2kFKHtthJ93tG
-                    ApproveOrderView(viewModel, onUseTestCardClick = {
-                        navController.navigate(DemoAppDestinations.SELECT_TEST_CARD)
-                    })
+                    ApproveOrderView(
+                        viewModel = viewModel,
+                        onUseTestCardClick = { navController.navigate(DemoAppDestinations.SELECT_TEST_CARD) }
+                    )
+                }
+                composable(DemoAppDestinations.CARD_VAULT) { entry ->
+                    val viewModel: VaultCardViewModel = hiltViewModel()
+
+                    // prefill test card (if necessary)
+                    entry.savedStateHandle.get<String>("test_card_id")?.let { testCardId ->
+                        TestCard.byId(testCardId)?.let { testCard ->
+                            viewModel.prefillCard(testCard)
+                        }
+                    }
+                    VaultCardView(
+                        viewModel = viewModel,
+                        onUseTestCardClick = { navController.navigate(DemoAppDestinations.SELECT_TEST_CARD) }
+                    )
                 }
                 composable(DemoAppDestinations.SELECT_TEST_CARD) {
                     SelectCardView(onTestCardSelected = { testCardId ->
