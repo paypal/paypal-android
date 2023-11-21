f013484a3e5dd4b1ba109e55f2cd50b1627c4958
sshropshire <sshropshire@paypal.com>
2023-11-21 11:06:45 -0600

    Remove ApproveOrderFragment.

--- Demo/src/main/java/com/paypal/android/MainActivity.kt
+++ Demo/src/main/java/com/paypal/android/MainActivity.kt
@@ -3,12 +3,16 @@ package com.paypal.android
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.material3.ExperimentalMaterial3Api
 import com.paypal.android.ui.DemoApp
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
+    @ExperimentalMaterial3Api
+    @ExperimentalFoundationApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
