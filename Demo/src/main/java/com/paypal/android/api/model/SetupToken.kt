8744e9b0bc01f669628af9ee9232102e6c61732f
sshropshire <sshropshire@paypal.com>
2023-11-28 14:24:13 -0600

    Integrate PayPal Web Vault view into Demo app.

--- Demo/src/main/java/com/paypal/android/api/model/SetupToken.kt
+++ Demo/src/main/java/com/paypal/android/api/model/SetupToken.kt
@@ -3,5 +3,6 @@ package com.paypal.android.api.model
 data class SetupToken(
     val id: String,
     val customerId: String,
-    val status: String
+    val status: String,
+    val approveVaultHref: String? = null
 )
