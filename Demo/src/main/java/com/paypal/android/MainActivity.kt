de5e9dac0a3a983c71dad698539a7cfee7d42e78
sshropshire <sshropshire@paypal.com>
2023-12-05 10:29:36 -0600

    Change MainActivity superclass to Component activity.

--- Demo/src/main/java/com/paypal/android/MainActivity.kt
+++ Demo/src/main/java/com/paypal/android/MainActivity.kt
@@ -3,9 +3,7 @@ package com.paypal.android
 import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
-import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.material3.ExperimentalMaterial3Api
 import com.paypal.android.ui.DemoApp
