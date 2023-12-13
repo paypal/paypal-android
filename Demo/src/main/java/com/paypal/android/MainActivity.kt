f1c20dbd94b4d3ead342a837497d9b39562079c8
sshropshire <sshropshire@paypal.com>
2023-12-13 13:56:47 -0600

    Revert to AppCompatActivty until our app supports ComponentActivity.

--- Demo/src/main/java/com/paypal/android/MainActivity.kt
+++ Demo/src/main/java/com/paypal/android/MainActivity.kt
@@ -4,13 +4,14 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.material3.ExperimentalMaterial3Api
 import com.paypal.android.ui.DemoApp
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
 
     @ExperimentalMaterial3Api
     @ExperimentalFoundationApi
