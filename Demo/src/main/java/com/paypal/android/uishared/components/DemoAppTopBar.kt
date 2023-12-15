798b66da528c22e7c4e7a027a5f302964309f36e
sshropshire <sshropshire@paypal.com>
2023-12-15 10:41:01 -0600

    Explicitly define styles for top app bar.

--- Demo/src/main/java/com/paypal/android/uishared/components/DemoAppTopBar.kt
+++ Demo/src/main/java/com/paypal/android/uishared/components/DemoAppTopBar.kt
@@ -1,13 +1,18 @@
 package com.paypal.android.uishared.components
 
+import androidx.compose.foundation.background
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarColors
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 
 @ExperimentalMaterial3Api
 @Composable
@@ -20,6 +25,10 @@ fun DemoAppTopBar(
         title = {
             Text(text = title)
         },
+        colors = TopAppBarDefaults.topAppBarColors(
+            containerColor = MaterialTheme.colorScheme.surface,
+            titleContentColor = MaterialTheme.colorScheme.onSurface
+        ),
         navigationIcon = {
             // Ref: https://stackoverflow.com/a/70409412
             if (shouldDisplayBackButton) {
@@ -30,6 +39,8 @@ fun DemoAppTopBar(
                     )
                 }
             }
-        }
+        },
+        modifier = Modifier
+            .background(MaterialTheme.colorScheme.surface)
     )
 }
