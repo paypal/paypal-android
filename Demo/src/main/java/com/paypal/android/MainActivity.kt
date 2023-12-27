0ac0a91eafc80512bc0a2f8466a716607ac90a6e
sshropshire <sshropshire@paypal.com>
2023-12-27 13:08:58 -0600

    Add additional annotation to MainActivity to resolve compilation error.

--- Demo/src/main/java/com/paypal/android/MainActivity.kt
+++ Demo/src/main/java/com/paypal/android/MainActivity.kt
@@ -6,12 +6,14 @@ import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.ui.ExperimentalComposeUiApi
 import com.paypal.android.ui.DemoApp
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
+    @ExperimentalComposeUiApi
     @ExperimentalMaterial3Api
     @ExperimentalFoundationApi
     override fun onCreate(savedInstanceState: Bundle?) {
