f874b9026f2743dc3f6595c9278497bbd91abea7
sshropshire <sshropshire@paypal.com>
2023-12-27 13:43:48 -0600

    Remove unused imports.

--- Demo/src/main/java/com/paypal/android/uishared/components/CreateOrderWithShippingPreferenceForm.kt
+++ Demo/src/main/java/com/paypal/android/uishared/components/CreateOrderWithShippingPreferenceForm.kt
@@ -1,17 +1,13 @@
 package com.paypal.android.uishared.components
 
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.paypal.android.R
 import com.paypal.android.api.model.OrderIntent
 import com.paypal.android.ui.paypalnative.ShippingPreferenceType
