a58dc4aae8103734051273347c7248a8e4784d12
sshropshire <sshropshire@paypal.com>
2024-01-04 16:16:15 -0600

    Fix lint errors.

--- Demo/src/main/java/com/paypal/android/uishared/components/PayPalPaymentTokenView.kt
+++ Demo/src/main/java/com/paypal/android/uishared/components/PayPalPaymentTokenView.kt
@@ -1,14 +1,9 @@
 package com.paypal.android.uishared.components
 
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedCard
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import com.paypal.android.api.model.PayPalPaymentToken
 import com.paypal.android.utils.UIConstants
 
