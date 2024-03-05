1053418338e11096f338555cb0158fea1a8e4185
sshropshire <sshropshire@paypal.com>
2024-03-05 10:32:51 -0600

    Remove parcelable conformance from CardVaultResult.

--- CardPayments/src/main/java/com/paypal/android/cardpayments/CardVaultResult.kt
+++ CardPayments/src/main/java/com/paypal/android/cardpayments/CardVaultResult.kt
@@ -11,11 +11,10 @@ import kotlinx.parcelize.Parcelize
  * @param setupTokenId the id for the setup token that was recently updated
  * @param status the status of the updated setup token
  */
-@Parcelize
 data class CardVaultResult(
     val setupTokenId: String,
     val status: String,
     // NOTE: This technically needs to null by default to prevent a breaking change
     // NEXT MAJOR VERSION: make `CardVaultResult` constructor private
     val authChallenge: CardAuthChallenge? = null
-) : Parcelable
+)
