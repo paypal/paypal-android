e4220248325c59fd5c645878b38bad993f86a186
sshropshire <sshropshire@paypal.com>
2024-03-05 11:48:23 -0600

    Add vault notification tests for paypal web checkout client.

--- PayPalWebPayments/src/test/java/com/paypal/android/paypalwebpayments/PayPalWebCheckoutClientUnitTest.kt
+++ PayPalWebPayments/src/test/java/com/paypal/android/paypalwebpayments/PayPalWebCheckoutClientUnitTest.kt
@@ -23,6 +23,9 @@ class PayPalWebCheckoutClientUnitTest {
 
     private val analyticsService = mockk<AnalyticsService>(relaxed = true)
 
+    private val checkoutListener = mockk<PayPalWebCheckoutListener>(relaxed = true)
+    private val vaultListener = mockk<PayPalWebVaultListener>(relaxed = true)
+
     private lateinit var payPalWebLauncher: PayPalWebLauncher
     private lateinit var sut: PayPalWebCheckoutClient
 
@@ -34,19 +37,19 @@ class PayPalWebCheckoutClientUnitTest {
 
     @Test
     fun `start() launches PayPal web checkout`() {
-        sut.listener = mockk(relaxed = true)
+        sut.listener = checkoutListener
 
         every { payPalWebLauncher.launchPayPalWebCheckout(any(), any()) } returns null
 
         val request = PayPalWebCheckoutRequest("fake-order-id")
         sut.start(request)
         verify(exactly = 1) { payPalWebLauncher.launchPayPalWebCheckout(activity, request) }
-        verify(exactly = 0) { sut.listener?.onPayPalWebFailure(any()) }
+        verify(exactly = 0) { checkoutListener.onPayPalWebFailure(any()) }
     }
 
     @Test
     fun `start() notifies merchant of browser switch failure`() {
-        sut.listener = mockk(relaxed = true)
+        sut.listener = checkoutListener
 
         val sdkError = PayPalSDKError(123, "fake error description")
         every { payPalWebLauncher.launchPayPalWebCheckout(any(), any()) } returns sdkError
@@ -55,14 +58,14 @@ class PayPalWebCheckoutClientUnitTest {
         sut.start(request)
 
         val slot = slot<PayPalSDKError>()
-        verify(exactly = 1) { sut.listener?.onPayPalWebFailure(capture(slot)) }
+        verify(exactly = 1) { checkoutListener.onPayPalWebFailure(capture(slot)) }
 
         assertSame(slot.captured, sdkError)
     }
 
     @Test
     fun `vault() launches PayPal web checkout`() {
-        sut.vaultListener = mockk(relaxed = true)
+        sut.vaultListener = vaultListener
 
         every { payPalWebLauncher.launchPayPalWebVault(any(), any()) } returns null
 
@@ -70,12 +73,12 @@ class PayPalWebCheckoutClientUnitTest {
             PayPalWebVaultRequest("fake-setup-token-id", "https://example.com/approval/url")
         sut.vault(request)
         verify(exactly = 1) { payPalWebLauncher.launchPayPalWebVault(activity, request) }
-        verify(exactly = 0) { sut.vaultListener?.onPayPalWebVaultFailure(any()) }
+        verify(exactly = 0) { vaultListener.onPayPalWebVaultFailure(any()) }
     }
 
     @Test
     fun `vault() notifies merchant of browser switch failure`() {
-        sut.vaultListener = mockk(relaxed = true)
+        sut.vaultListener = vaultListener
 
         val sdkError = PayPalSDKError(123, "fake error description")
         every { payPalWebLauncher.launchPayPalWebVault(any(), any()) } returns sdkError
@@ -85,14 +88,14 @@ class PayPalWebCheckoutClientUnitTest {
         sut.vault(request)
 
         val slot = slot<PayPalSDKError>()
-        verify(exactly = 1) { sut.vaultListener?.onPayPalWebVaultFailure(capture(slot)) }
+        verify(exactly = 1) { vaultListener.onPayPalWebVaultFailure(capture(slot)) }
 
         assertSame(slot.captured, sdkError)
     }
 
     @Test
     fun `handleBrowserSwitchResult notifies merchant of Checkout success`() {
-        sut.listener = mockk(relaxed = true)
+        sut.listener = checkoutListener
 
         val successResult = PayPalWebCheckoutResult("fake-order-id", "fake-payer-id")
         every {
@@ -102,13 +105,13 @@ class PayPalWebCheckoutClientUnitTest {
         sut.handleBrowserSwitchResult()
 
         val slot = slot<PayPalWebCheckoutResult>()
-        verify(exactly = 1) { sut.listener?.onPayPalWebSuccess(capture(slot)) }
+        verify(exactly = 1) { checkoutListener.onPayPalWebSuccess(capture(slot)) }
         assertSame(successResult, slot.captured)
     }
 
     @Test
     fun `handleBrowserSwitchResult notifies merchant of Checkout failure`() {
-        sut.listener = mockk(relaxed = true)
+        sut.listener = checkoutListener
 
         val error = PayPalSDKError(123, "fake-error-description")
         every {
@@ -118,13 +121,13 @@ class PayPalWebCheckoutClientUnitTest {
         sut.handleBrowserSwitchResult()
 
         val slot = slot<PayPalSDKError>()
-        verify(exactly = 1) { sut.listener?.onPayPalWebFailure(capture(slot)) }
+        verify(exactly = 1) { checkoutListener.onPayPalWebFailure(capture(slot)) }
         assertSame(error, slot.captured)
     }
 
     @Test
     fun `handleBrowserSwitchResult notifies merchant of Checkout cancelation`() {
-        sut.listener = mockk(relaxed = true)
+        sut.listener = checkoutListener
 
         val status = PayPalWebStatus.CheckoutCanceled("fake-order_id")
         every {
@@ -133,17 +136,62 @@ class PayPalWebCheckoutClientUnitTest {
 
         sut.handleBrowserSwitchResult()
 
-        verify(exactly = 1) { sut.listener?.onPayPalWebCanceled() }
+        verify(exactly = 1) { checkoutListener.onPayPalWebCanceled() }
+    }
+
+    @Test
+    fun `handleBrowserSwitchResult notifies merchant of vault success`() {
+        sut.vaultListener = vaultListener
+
+        val successResult = PayPalWebVaultResult("fake-approval-session-id")
+        every {
+            payPalWebLauncher.deliverBrowserSwitchResult(activity)
+        } returns PayPalWebStatus.VaultSuccess(successResult)
+
+        sut.handleBrowserSwitchResult()
+
+        val slot = slot<PayPalWebVaultResult>()
+        verify(exactly = 1) { vaultListener.onPayPalWebVaultSuccess(capture(slot)) }
+        assertSame(successResult, slot.captured)
+    }
+
+    @Test
+    fun `handleBrowserSwitchResult notifies merchant of vault failure`() {
+        sut.vaultListener = vaultListener
+
+        val error = PayPalSDKError(123, "fake-error-description")
+        every {
+            payPalWebLauncher.deliverBrowserSwitchResult(activity)
+        } returns PayPalWebStatus.VaultError(error)
+
+        sut.handleBrowserSwitchResult()
+
+        val slot = slot<PayPalSDKError>()
+        verify(exactly = 1) { vaultListener.onPayPalWebVaultFailure(capture(slot)) }
+        assertSame(error, slot.captured)
+    }
+
+    @Test
+    fun `handleBrowserSwitchResult notifies merchant of vault cancelation`() {
+        sut.vaultListener = vaultListener
+
+        every {
+            payPalWebLauncher.deliverBrowserSwitchResult(activity)
+        } returns PayPalWebStatus.VaultCanceled
+
+        sut.handleBrowserSwitchResult()
+        verify(exactly = 1) { vaultListener.onPayPalWebVaultCanceled() }
     }
 
     @Test
     fun `handleBrowserSwitchResult doesn't deliver result when browserSwitchResult is null`() {
-        sut.listener = mockk(relaxed = true)
+        sut.listener = checkoutListener
+        sut.vaultListener = vaultListener
         every { payPalWebLauncher.deliverBrowserSwitchResult(activity) } returns null
 
         sut.handleBrowserSwitchResult()
-        verify { sut.listener?.wasNot(Called) }
-        verify { sut.vaultListener?.wasNot(Called) }
+        verify { checkoutListener.wasNot(Called) }
+        verify { vaultListener.wasNot(Called) }
     }
 
     @Test
