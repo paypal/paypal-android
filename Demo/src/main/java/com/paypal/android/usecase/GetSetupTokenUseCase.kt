496ca3fb5b324e5e69b89da2c02c39309ab4b43a
sshropshire <sshropshire@paypal.com>
2024-03-05 13:56:37 -0600

    Add GetSetupTokenUseCase to project.

---
+++ Demo/src/main/java/com/paypal/android/usecase/GetSetupTokenUseCase.kt
@@ -0,0 +1,19 @@
+package com.paypal.android.usecase
+
+import com.paypal.android.api.model.CardSetupToken
+import com.paypal.android.api.services.SDKSampleServerAPI
+import com.paypal.android.api.services.SDKSampleServerResult
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import javax.inject.Inject
+
+class GetSetupTokenUseCase @Inject constructor(
+    private val sdkSampleServerAPI: SDKSampleServerAPI
+) {
+
+    // TODO: pass SCA enum as parameter instead of perform3DS
+    suspend operator fun invoke(setupTokenId: String): SDKSampleServerResult<CardSetupToken, Exception> =
+        withContext(Dispatchers.IO) {
+            sdkSampleServerAPI.getSetupToken(setupTokenId)
+        }
+}
