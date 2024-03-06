4727f34b8a6d7d0eb0f2914cb8ad82232d392766
sshropshire <sshropshire@paypal.com>
2024-03-06 16:14:54 -0600

    Remove unused imports in InfoColumn.

--- Demo/src/main/java/com/paypal/android/uishared/components/InfoColumn.kt
+++ Demo/src/main/java/com/paypal/android/uishared/components/InfoColumn.kt
@@ -1,23 +1,18 @@
 package com.paypal.android.uishared.components
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import com.paypal.android.utils.UIConstants
-import org.intellij.lang.annotations.JdkConstants.HorizontalAlignment
 
 @Composable
 fun InfoColumn(
