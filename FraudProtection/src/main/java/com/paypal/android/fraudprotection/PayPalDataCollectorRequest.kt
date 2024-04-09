da06b0a30c80eb277c09a1211918c86394563f20
sshropshire <58225613+sshropshire@users.noreply.github.com>
2024-04-09 10:00:54 -0500

    Update FraudProtection/src/main/java/com/paypal/android/fraudprotection/PayPalDataCollectorRequest.kt
    Co-authored-by: Sarah Koop <skoop@paypal.com>

--- FraudProtection/src/main/java/com/paypal/android/fraudprotection/PayPalDataCollectorRequest.kt
+++ FraudProtection/src/main/java/com/paypal/android/fraudprotection/PayPalDataCollectorRequest.kt
@@ -3,9 +3,10 @@ package com.paypal.android.fraudprotection
 /**
  * Request object containing parameters to configure fraud protection data collection.
  *
- * @property [hasUserLocationConsent] true if the user has explicitly granted permission for a
- * merchant app to collect device data to protect against fraud; false if explicit permission was
- * not granted (default).
+ * @property [hasUserLocationConsent] informs the SDK if your application has obtained
+ * consent from the user to collect location data in compliance with 
+ * <a href="https://support.google.com/googleplay/android-developer/answer/10144311#personal-sensitive">Google Play Developer Program policies</a> 
+ * This flag enables PayPal to collect necessary information required for Fraud Detection and Risk Management. 
  * @property [clientMetadataId] forward this data to your server when completing a transaction
  * @property [additionalData] additional metadata to link with data collection
  */
