20952f9d04d0679cbe45f16c89a8cd17df753cbc
sshropshire <sshropshire@paypal.com>
2024-04-10 10:00:29 -0500

    Fix lint errors.

--- FraudProtection/src/main/java/com/paypal/android/fraudprotection/PayPalDataCollectorRequest.kt
+++ FraudProtection/src/main/java/com/paypal/android/fraudprotection/PayPalDataCollectorRequest.kt
@@ -4,9 +4,10 @@ package com.paypal.android.fraudprotection
  * Request object containing parameters to configure fraud protection data collection.
  *
  * @property [hasUserLocationConsent] informs the SDK if your application has obtained
- * consent from the user to collect location data in compliance with 
- * <a href="https://support.google.com/googleplay/android-developer/answer/10144311#personal-sensitive">Google Play Developer Program policies</a> 
- * This flag enables PayPal to collect necessary information required for Fraud Detection and Risk Management. 
+ * consent from the user to collect location data in compliance with
+ * <a href="https://support.google.com/googleplay/android-developer/answer/10144311#personal-sensitive">
+ * Google Play Developer Program policies</a>
+ * This flag enables PayPal to collect necessary information required for Fraud Detection and Risk Management.
  * @property [clientMetadataId] forward this data to your server when completing a transaction
  * @property [additionalData] additional metadata to link with data collection
  */
