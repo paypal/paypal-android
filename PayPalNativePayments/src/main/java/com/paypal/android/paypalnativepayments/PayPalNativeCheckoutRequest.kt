d4d2b7818471a5a0d804666419fa57fdc29a6976
saperi <saperi@paypal.com>
2024-04-22 10:08:13 -0700

    add deprecation message to constructors

--- PayPalNativePayments/src/main/java/com/paypal/android/paypalnativepayments/PayPalNativeCheckoutRequest.kt
+++ PayPalNativePayments/src/main/java/com/paypal/android/paypalnativepayments/PayPalNativeCheckoutRequest.kt
@@ -23,4 +23,19 @@ data class PayPalNativeCheckoutRequest(
      * This flag enables PayPal to collect necessary information required for Fraud Detection and Risk Management.
      */
     val hasUserLocationConsent: Boolean = false
-)
+) {
+    @Deprecated(
+        message = "Use the constructor to pass in the `hasUserLocationConsent` parameter.",
+        replaceWith = ReplaceWith("PayPalNativeCheckoutRequest(orderId, hasUserLocationConsent)")
+    )
+    constructor(orderId: String) : this(orderId, null, false)
+
+    @Deprecated("Use the constructor to pass in the `hasUserLocationConsent` parameter.",
+        replaceWith = ReplaceWith("PayPalNativeCheckoutRequest(orderId, userAuthenticationEmail, hasUserLocationConsent)")
+    )
+    constructor(orderId: String, userAuthenticationEmail: String?) : this(
+        orderId,
+        userAuthenticationEmail,
+        false
+    )
+}
