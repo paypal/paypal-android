d71f05bb413d6204b6d3bf3083679894272390e9
sshropshire <sshropshire@paypal.com>
2024-07-09 15:32:04 -0500

    Add VenmoClientUnitTest to project.

---
+++ VenmoPayments/src/test/java/com/paypal/android/venmopayments/VenmoClientUnitTest.kt
@@ -0,0 +1,12 @@
+package com.paypal.android.venmopayments
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class VenmoClientUnitTest {
+
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
