039c1b7808a93059357ffe61ecea5c3a58601cb3
sshropshire <sshropshire@paypal.com>
2024-07-09 15:33:39 -0500

    Remove example unit test.

--- VenmoPayments/src/test/java/com/paypal/android/venmopayments/VenmoClientUnitTest.kt
+++ VenmoPayments/src/test/java/com/paypal/android/venmopayments/VenmoClientUnitTest.kt
@@ -1,12 +1,13 @@
 package com.paypal.android.venmopayments
 
-import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNotNull
 import org.junit.Test
 
 class VenmoClientUnitTest {
 
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun exists() {
+        val sut = VenmoClient()
+        assertNotNull(sut)
     }
 }
