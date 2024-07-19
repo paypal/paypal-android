b3e344b84bfde3350e16c29542678482d0ae82e6
sshropshire <sshropshire@paypal.com>
2024-07-19 10:59:14 -0500

    Add reverted ElibilityAPI related src files to project.

---
+++ CorePayments/src/main/java/com/paypal/android/corepayments/JSONObjectExt.kt
@@ -0,0 +1,16 @@
+package com.paypal.android.corepayments
+
+import org.json.JSONObject
+
+fun JSONObject.optBooleanAtKeyPath(keyPath: String, fallback: Boolean = false): Boolean {
+    val keys = keyPath.split(".").toMutableList()
+    if (keys.isNotEmpty()) {
+        var targetObject: JSONObject = this
+        while (keys.size > 1) {
+            val key = keys.removeFirst()
+            targetObject = targetObject.optJSONObject(key) ?: JSONObject()
+        }
+        return targetObject.optBoolean(keys[0], fallback)
+    }
+    return fallback
+}
---
+++ CorePayments/src/main/java/com/paypal/android/corepayments/graphql/fundingEligibility/models/SupportedCountryCurrencyType.kt
@@ -0,0 +1,5 @@
+package com.paypal.android.corepayments.graphql.fundingEligibility.models
+
+internal enum class SupportedCountryCurrencyType {
+    USD
+}
