74e525fee0ef8bea2693704805884493ad1c05a1
sshropshire <sshropshire@paypal.com>
2024-07-19 15:48:20 -0500

    Fix lint error.

--- Demo/src/main/java/com/paypal/android/ui/venmocheckout/VenmoCheckoutViewModel.kt
+++ Demo/src/main/java/com/paypal/android/ui/venmocheckout/VenmoCheckoutViewModel.kt
@@ -4,9 +4,10 @@ import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import javax.inject.Inject
 
 @HiltViewModel
-class VenmoCheckoutViewModel : ViewModel() {
+class VenmoCheckoutViewModel @Inject constructor() : ViewModel() {
 
     companion object {
         const val TAG = "VenmoViewModel"
