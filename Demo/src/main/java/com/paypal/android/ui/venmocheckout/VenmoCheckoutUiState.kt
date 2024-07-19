801e5433b7d5e0b954eb40ee974ff31d488b68a6
sshropshire <sshropshire@paypal.com>
2024-07-19 15:49:06 -0500

    Fix typo in comment.

--- Demo/src/main/java/com/paypal/android/ui/venmocheckout/VenmoCheckoutUiState.kt
+++ Demo/src/main/java/com/paypal/android/ui/venmocheckout/VenmoCheckoutUiState.kt
@@ -3,7 +3,7 @@ package com.paypal.android.ui.venmocheckout
 import com.paypal.android.uishared.state.ActionState
 
 data class VenmoCheckoutUiState(
-    // TODO: replace once EligibilityResult is emplemented
+    // TODO: replace once EligibilityResult is implemented
 //    val checkEligibilityState: ActionState<EligibilityResult, Exception> = ActionState.Idle,
     val checkEligibilityState: ActionState<Void, Exception> = ActionState.Idle,
 )
