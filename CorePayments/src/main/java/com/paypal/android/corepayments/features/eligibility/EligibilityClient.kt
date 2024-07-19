a6d334239e8dc1e3821effc0500fb9ac2bbb5583
sshropshire <sshropshire@paypal.com>
2024-07-19 14:56:55 -0500

    Update Eligibility client constructor to take a context.

--- CorePayments/src/main/java/com/paypal/android/corepayments/features/eligibility/EligibilityClient.kt
+++ CorePayments/src/main/java/com/paypal/android/corepayments/features/eligibility/EligibilityClient.kt
@@ -10,20 +10,25 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
 class EligibilityClient internal constructor(
+    private val applicationContext: Context,
     private val eligibilityAPI: EligibilityAPI,
     private val dispatcher: CoroutineDispatcher
 ) {
 
-    constructor(coreConfig: CoreConfig) : this(EligibilityAPI(coreConfig), Dispatchers.Main)
+    constructor(context: Context, coreConfig: CoreConfig) : this(
+        context.applicationContext,
+        EligibilityAPI(coreConfig),
+        Dispatchers.Main
+    )
 
     fun check(
-        context: Context,
         eligibilityRequest: EligibilityRequest,
         callback: CheckEligibilityResult
     ) {
         CoroutineScope(dispatcher).launch {
             val eligibilityResult = try {
-                val response = eligibilityAPI.checkEligibility(context, eligibilityRequest)
+                val response =
+                    eligibilityAPI.checkEligibility(applicationContext, eligibilityRequest)
                 EligibilityResult.Success(isVenmoEligible = response.isVenmoEligible)
             } catch (e: PayPalSDKError) {
                 EligibilityResult.Failure(e)
