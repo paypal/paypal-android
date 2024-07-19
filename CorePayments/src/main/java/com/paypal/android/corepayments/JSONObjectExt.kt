0cb4a7e2fe40511613bf4c4dbcaac5c4a9773dc7
sshropshire <sshropshire@paypal.com>
2024-07-19 11:56:54 -0500

    Update comment.

--- CorePayments/src/main/java/com/paypal/android/corepayments/JSONObjectExt.kt
+++ CorePayments/src/main/java/com/paypal/android/corepayments/JSONObjectExt.kt
@@ -5,12 +5,13 @@ import org.json.JSONObject
 fun JSONObject.optBooleanAtKeyPath(keyPath: String, fallback: Boolean = false): Boolean {
     val keys = keyPath.split(".").toMutableList()
     if (keys.isNotEmpty()) {
-        var targetObject: JSONObject = this
+        // with _this_ as the root JSON node, iteratively search the key path until we reach the last key
+        var targetObject: JSONObject? = this
         while (keys.size > 1) {
             val key = keys.removeFirst()
-            targetObject = targetObject.optJSONObject(key) ?: JSONObject()
+            targetObject = targetObject?.optJSONObject(key)
         }
-        return targetObject.optBoolean(keys[0], fallback)
+        return targetObject?.optBoolean(keys[0]) ?: fallback
     }
     return fallback
 }
