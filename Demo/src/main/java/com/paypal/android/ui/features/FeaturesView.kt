6409ea7fddef49f31be3e588e8595bfb209f2ccc
sshropshire <sshropshire@paypal.com>
2024-07-19 15:30:04 -0500

    Fix detekt lint.

--- Demo/src/main/java/com/paypal/android/ui/features/FeaturesView.kt
+++ Demo/src/main/java/com/paypal/android/ui/features/FeaturesView.kt
@@ -1,6 +1,5 @@
 package com.paypal.android.ui.features
 
-import androidx.annotation.UiContext
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
@@ -11,7 +10,6 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.safeContentPadding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Card
@@ -24,7 +22,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.semantics.Role
