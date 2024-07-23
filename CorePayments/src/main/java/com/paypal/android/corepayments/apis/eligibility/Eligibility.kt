1280f4a6157e6eb31a865530abc7522f4e832ed9
sshropshire <sshropshire@paypal.com>
2024-07-23 11:22:25 -0500

    Remove docstring on internal type.

--- CorePayments/src/main/java/com/paypal/android/corepayments/apis/eligibility/Eligibility.kt
+++ CorePayments/src/main/java/com/paypal/android/corepayments/apis/eligibility/Eligibility.kt
@@ -1,8 +1,5 @@
 package com.paypal.android.corepayments.apis.eligibility
 
-/**
- *  Class representing eligibility for a set of payment methods
- */
 internal data class Eligibility(
     val isVenmoEligible: Boolean,
     val isPaypalEligible: Boolean,
