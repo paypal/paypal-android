01de719aa5fa4836cc5b8ca9f80747307d7f0f89
sshropshire <sshropshire@paypal.com>
2024-07-23 11:03:44 -0500

    Update EligibilityRequest and ElibilityResult doc strings.

--- CorePayments/src/main/java/com/paypal/android/corepayments/features/eligibility/EligibilityResult.kt
+++ CorePayments/src/main/java/com/paypal/android/corepayments/features/eligibility/EligibilityResult.kt
@@ -1,3 +1,6 @@
 package com.paypal.android.corepayments.features.eligibility
 
+/**
+ * Contains the result of a successful [EligibilityClient.check] response.
+ */
 data class EligibilityResult (val isVenmoEligible: Boolean)
