0ee90113b06e002b0a7b271d776444d4dbc0e26a
sshropshire <58225613+sshropshire@users.noreply.github.com>
2024-07-24 11:42:01 -0500

    Update CorePayments/src/main/java/com/paypal/android/corepayments/apis/eligibility/FundingEligibilityIntent.kt
    Co-authored-by: Jax DesMarais-Leder <jdesmarais@paypal.com>

--- CorePayments/src/main/java/com/paypal/android/corepayments/apis/eligibility/FundingEligibilityIntent.kt
+++ CorePayments/src/main/java/com/paypal/android/corepayments/apis/eligibility/FundingEligibilityIntent.kt
@@ -1,5 +1,5 @@
 package com.paypal.android.corepayments.apis.eligibility
 
 internal enum class FundingEligibilityIntent {
-    SALE, CAPTURE, AUTHORIZE, ORDER, TOKENIZE, SUBSCRIPTION
+    CAPTURE, AUTHORIZE
 }
