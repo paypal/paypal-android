2ecd13823671ccbd6c9353f4621bcee0a6f08bc9
sshropshire <sshropshire@paypal.com>
2024-10-15 10:46:14 -0500

    Add Error Dialog to CheckoutView.

--- Demo/src/main/java/com/paypal/android/plainclothes/ErrorDialog.kt
+++ Demo/src/main/java/com/paypal/android/plainclothes/ErrorDialog.kt
@@ -1,29 +1,28 @@
 package com.paypal.android.plainclothes
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.window.Dialog
-import androidx.compose.ui.window.DialogProperties
 
 @Composable
 fun ErrorDialog(error: Throwable, onDismissRequest: () -> Unit) {
-    Dialog(onDismissRequest = onDismissRequest) {
-        Box(
-            contentAlignment = Alignment.Center,
-            modifier = Modifier
-                .background(Color.White, shape = RoundedCornerShape(8.dp))
-                .padding(48.dp)
-        ) {
+    AlertDialog(
+        onDismissRequest = onDismissRequest,
+        confirmButton = {
+            TextButton(
+                onClick = {
+                    onDismissRequest()
+                }
+            ) {
+                Text("OK")
+            }
+        },
+        title = {
+            Text("An Error Occurred")
+        },
+        text = {
             Text(text = error.message ?: "An Error Occurred")
         }
-    }
+    )
 }
