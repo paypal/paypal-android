32f533f78652c28b50136ccec9a2b17bdb884798
sshropshire <sshropshire@paypal.com>
2024-11-19 15:04:34 -0600

    Remove url parsing from 3DS deep link return URLs to match feature parity with iOS.

--- CardPayments/src/main/java/com/paypal/android/cardpayments/CardResult.kt
+++ CardPayments/src/main/java/com/paypal/android/cardpayments/CardResult.kt
@@ -1,29 +1,15 @@
 package com.paypal.android.cardpayments
 
-import android.net.Uri
-
 /**
  * A result returned by [CardClient] when an order was successfully approved with a [Card].
  *
  * @property [orderId] associated order ID.
- * @property [liabilityShift] Liability shift value returned from 3DS verification
  * @property [status] status of the order
  * @property [didAttemptThreeDSecureAuthentication] 3DS verification was attempted.
  * Use v2/checkout/orders/{orderId} in your server to get verification results.
  */
-data class CardResult(
+data class CardResult internal constructor(
     val orderId: String,
-
-    /**
-     * @suppress
-     */
-    @Deprecated("Use status instead.")
-    val deepLinkUrl: Uri? = null,
-
-    @Deprecated("Use didAttemptThreeDSecureAuthentication instead.")
-    val liabilityShift: String? = null,
-
     val status: String? = null,
-
     val didAttemptThreeDSecureAuthentication: Boolean = false
 )
--- CardPayments/src/main/java/com/paypal/android/cardpayments/CardVaultResult.kt
+++ CardPayments/src/main/java/com/paypal/android/cardpayments/CardVaultResult.kt
@@ -1,15 +1,15 @@
 package com.paypal.android.cardpayments
 
 /**
- * @suppress
- *
  * A result returned by [CardClient] when an a successful vault occurs.
  *
  * @param setupTokenId the id for the setup token that was recently updated
  * @param status the status of the updated setup token
+ * @property [didAttemptThreeDSecureAuthentication] 3DS verification was attempted.
+ * Use v2/checkout/orders/{orderId} in your server to get verification results.
  */
-// NEXT MAJOR VERSION: make `CardVaultResult` constructor private
-data class CardVaultResult(
+data class CardVaultResult internal constructor(
     val setupTokenId: String,
-    val status: String,
+    val status: String? = null,
+    val didAttemptThreeDSecureAuthentication: Boolean = false
 )
