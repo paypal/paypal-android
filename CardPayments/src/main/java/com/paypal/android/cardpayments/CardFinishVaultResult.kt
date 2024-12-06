9691686f9ab052bfe96ae94c70d0d55c568df608
sshropshire <sshropshire@paypal.com>
2024-12-06 15:38:30 -0600

    Add CardAuthLauncher completeVaultAuthRequest method.

---
+++ CardPayments/src/main/java/com/paypal/android/cardpayments/CardFinishVaultResult.kt
@@ -0,0 +1,15 @@
+package com.paypal.android.cardpayments
+
+import com.paypal.android.corepayments.PayPalSDKError
+
+sealed class CardFinishVaultResult {
+
+    data class Success(
+        val setupTokenId: String,
+        val status: String? = null,
+        val didAttemptThreeDSecureAuthentication: Boolean = false
+    ): CardFinishVaultResult()
+
+    data class Failure(val error: PayPalSDKError) : CardFinishVaultResult()
+    data object NoResult : CardFinishVaultResult()
+}
