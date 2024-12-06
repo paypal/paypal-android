f74e05bcc5b023f135d8c0f03502c5011d20b68d
sshropshire <sshropshire@paypal.com>
2024-12-06 15:15:21 -0600

    Add CardVaultResult success type.

---
+++ CardPayments/src/main/java/com/paypal/android/cardpayments/CardVaultResult.kt
@@ -0,0 +1,18 @@
+package com.paypal.android.cardpayments
+
+sealed class CardVaultResult {
+
+    /**
+     * A result returned by [CardClient] when an a successful vault occurs.
+     *
+     * @param setupTokenId the id for the setup token that was recently updated
+     * @param status the status of the updated setup token
+     * @property [didAttemptThreeDSecureAuthentication] 3DS verification was attempted.
+     * Use v2/checkout/orders/{orderId} in your server to get verification results.
+     */
+    data class Success(
+        val setupTokenId: String,
+        val status: String? = null,
+        val didAttemptThreeDSecureAuthentication: Boolean = false
+    )
+}
