5910b3152624657759e1e0a1b197772969755c72
sshropshire <sshropshire@paypal.com>
2024-12-10 11:35:12 -0600

    Remove PayPalWebStatus events for Checkout.

--- PayPalWebPayments/src/main/java/com/paypal/android/paypalwebpayments/PayPalWebStatus.kt
+++ PayPalWebPayments/src/main/java/com/paypal/android/paypalwebpayments/PayPalWebStatus.kt
@@ -4,10 +4,6 @@ import com.paypal.android.corepayments.PayPalSDKError
 
 sealed class PayPalWebStatus {
 
-    class CheckoutError(val error: PayPalSDKError, val orderId: String?) : PayPalWebStatus()
-    class CheckoutSuccess(val result: PayPalWebCheckoutResult) : PayPalWebStatus()
-    class CheckoutCanceled(val orderId: String?) : PayPalWebStatus()
-
     class VaultError(val error: PayPalSDKError) : PayPalWebStatus()
     class VaultSuccess(val result: PayPalWebVaultResult) : PayPalWebStatus()
     data object VaultCanceled : PayPalWebStatus()
