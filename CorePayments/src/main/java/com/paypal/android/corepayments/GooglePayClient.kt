034a453216e3d1e6202521f4e631db0523a146ad
sshropshire <sshropshire@paypal.com>
2025-03-26 10:25:29 -0500

    Add GooglePay view to project.

---
+++ CorePayments/src/main/java/com/paypal/android/corepayments/GooglePayClient.kt
@@ -0,0 +1,4 @@
+package com.paypal.android.corepayments
+
+class GooglePayClient {
+}
---
+++ Demo/src/main/java/com/paypal/android/ui/googlepay/GooglePayView.kt
@@ -0,0 +1,24 @@
+package com.paypal.android.ui.googlepay
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun GooglePayView() {
+    Text("Helloo")
+}
+
+@Preview
+@Composable
+fun GooglePayViewPreview() {
+    MaterialTheme {
+        Surface(modifier = Modifier.fillMaxSize()) {
+            GooglePayView()
+        }
+    }
+}
