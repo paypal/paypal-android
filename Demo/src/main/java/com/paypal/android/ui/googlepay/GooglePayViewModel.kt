e2cd4eb330712c9fab253c3bb18e793ded8afabc
sshropshire <sshropshire@paypal.com>
2025-03-26 10:47:03 -0500

    Tweak button to launch Google Pay.

---
+++ Demo/src/main/java/com/paypal/android/ui/googlepay/GooglePayViewModel.kt
@@ -0,0 +1,32 @@
+package com.paypal.android.ui.googlepay
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.paypal.android.api.services.SDKSampleServerResult
+import com.paypal.android.corepayments.CoreConfig
+import com.paypal.android.corepayments.GooglePayClient
+import com.paypal.android.usecase.GetClientIdUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class GooglePayViewModel @Inject constructor(
+    val getClientIdUseCase: GetClientIdUseCase,
+) : ViewModel() {
+
+    private var googlePayClient: GooglePayClient? = null
+
+    fun launchGooglePay() {
+        viewModelScope.launch {
+            when (val clientIdResult = getClientIdUseCase()) {
+                is SDKSampleServerResult.Failure -> TODO("handle failure")
+                is SDKSampleServerResult.Success -> {
+                    val coreConfig = CoreConfig(clientIdResult.value)
+                    googlePayClient = GooglePayClient(config = coreConfig)
+                    googlePayClient?.start()
+                }
+            }
+        }
+    }
+}
