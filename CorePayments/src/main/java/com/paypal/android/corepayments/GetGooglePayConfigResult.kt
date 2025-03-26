19cbf40b6fc5cffdd5bef205d0952ea22958709d
sshropshire <sshropshire@paypal.com>
2025-03-26 16:52:01 -0500

    Implement GooglePay presentment.

--- CorePayments/src/main/java/com/paypal/android/corepayments/GetGooglePayConfigResult.kt
+++ CorePayments/src/main/java/com/paypal/android/corepayments/GetGooglePayConfigResult.kt
@@ -1,6 +1,6 @@
 package com.paypal.android.corepayments
 
 sealed class GetGooglePayConfigResult {
-    data class Success(val config: GooglePayConfig) : GetGooglePayConfigResult()
+    data class Success(val value: GooglePayConfig) : GetGooglePayConfigResult()
     data class Failure(val error: PayPalSDKError) : GetGooglePayConfigResult()
 }
--- CorePayments/src/main/java/com/paypal/android/corepayments/GooglePayClient.kt
+++ CorePayments/src/main/java/com/paypal/android/corepayments/GooglePayClient.kt
@@ -1,22 +1,82 @@
 package com.paypal.android.corepayments
 
 import android.content.Context
-import android.util.Log
+import com.google.android.gms.tasks.Task
+import com.google.android.gms.wallet.PaymentData
+import com.google.android.gms.wallet.PaymentDataRequest
+import com.google.android.gms.wallet.PaymentsClient
+import com.google.android.gms.wallet.Wallet
+import com.google.android.gms.wallet.WalletConstants
+import org.json.JSONArray
+import org.json.JSONObject
 
 class GooglePayClient(
-    private val googlePayAPI: GooglePayAPI
+    private val googlePayAPI: GooglePayAPI,
+    private val paymentsClient: PaymentsClient
 ) {
     constructor(context: Context, config: CoreConfig) : this(
-        googlePayAPI = GooglePayAPI(context, config)
+        googlePayAPI = GooglePayAPI(context, config),
+        paymentsClient = createPaymentsClient(context)
     )
 
-    suspend fun start() {
-        when (val result = googlePayAPI.getGooglePayConfig()) {
+    suspend fun start(): Task<PaymentData> {
+        return when (val result = googlePayAPI.getGooglePayConfig()) {
             is GetGooglePayConfigResult.Failure -> TODO("handle error")
             is GetGooglePayConfigResult.Success -> {
-                val config = result.config
-                Log.d("GooglePayClient", "Google Pay Eligible: ${config.isEligible}")
+                loadPaymentData(result.value)
             }
+        }
+    }
+
+    private suspend fun loadPaymentData(googlePayConfig: GooglePayConfig): Task<PaymentData> {
+        if (googlePayConfig.isEligible) {
+            val paymentDataRequestJSON = createPaymentDataRequest(googlePayConfig)
+            val paymentDataRequest = PaymentDataRequest.fromJson(paymentDataRequestJSON.toString())
+            return paymentsClient.loadPaymentData(paymentDataRequest)
+        } else {
+            TODO("Handle GooglePay ineligibility")
+        }
+    }
+
+    private fun createPaymentDataRequest(googlePayConfig: GooglePayConfig): JSONObject {
+        val displayItems = JSONArray(
+            listOf(
+                JSONObject()
+                    .put("label", "Subtotal")
+                    .put("type", "SUBTOTAL")
+                    .put("price", "100.00"),
+                JSONObject()
+                    .put("label", "Tax")
+                    .put("type", "TAX")
+                    .put("price", "10.00"),
+            )
+        )
+
+        val transactionInfo = JSONObject()
+            .put("displayItems", displayItems)
+            .put("countryCode", "US")
+            .put("currencyCode", "USD")
+            .put("totalPriceStatus", "FINAL")
+            .put("totalPrice", "110.00")
+            .put("totalPriceLabel", "Total")
+
+        val request = JSONObject()
+            .put("apiVersion", 2)
+            .put("apiVersionMinor", 0)
+            .put("allowedPaymentMethods", googlePayConfig.allowedPaymentMethods)
+            .put("merchantInfo", googlePayConfig.merchantInfo)
+            .put("callbackIntents", JSONArray(listOf("PAYMENT_AUTHORIZATION")))
+            .put("transactionInfo", transactionInfo)
+        return request
+    }
+
+    companion object {
+        fun createPaymentsClient(context: Context): PaymentsClient {
+            val walletOptions = Wallet.WalletOptions.Builder()
+                .setEnvironment(WalletConstants.ENVIRONMENT_TEST)
+                .build()
+
+            return Wallet.getPaymentsClient(context, walletOptions)
         }
     }
 }
