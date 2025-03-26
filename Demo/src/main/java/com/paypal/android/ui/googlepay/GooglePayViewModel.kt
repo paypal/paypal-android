ef9832d57f248a000e77b85b9b1887f0cadb1968
sshropshire <sshropshire@paypal.com>
2025-03-26 16:52:01 -0500

    Implement GooglePay presentment.

--- Demo/src/main/java/com/paypal/android/ui/googlepay/GooglePayViewModel.kt
+++ Demo/src/main/java/com/paypal/android/ui/googlepay/GooglePayViewModel.kt
@@ -2,13 +2,13 @@ package com.paypal.android.ui.googlepay
 
 import androidx.activity.ComponentActivity
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
+import com.google.android.gms.tasks.Task
+import com.google.android.gms.wallet.PaymentData
 import com.paypal.android.api.services.SDKSampleServerResult
 import com.paypal.android.corepayments.CoreConfig
 import com.paypal.android.corepayments.GooglePayClient
 import com.paypal.android.usecase.GetClientIdUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -18,15 +18,13 @@ class GooglePayViewModel @Inject constructor(
 
     private var googlePayClient: GooglePayClient? = null
 
-    fun launchGooglePay(activity: ComponentActivity) {
-        viewModelScope.launch {
-            when (val clientIdResult = getClientIdUseCase()) {
-                is SDKSampleServerResult.Failure -> TODO("handle failure")
-                is SDKSampleServerResult.Success -> {
-                    val coreConfig = CoreConfig(clientIdResult.value)
-                    googlePayClient = GooglePayClient(activity, coreConfig)
-                    googlePayClient?.start()
-                }
+    suspend fun launchGooglePay(activity: ComponentActivity): Task<PaymentData> {
+        when (val clientIdResult = getClientIdUseCase()) {
+            is SDKSampleServerResult.Failure -> TODO("handle failure")
+            is SDKSampleServerResult.Success -> {
+                val coreConfig = CoreConfig(clientIdResult.value)
+                googlePayClient = GooglePayClient(activity, coreConfig)
+                return googlePayClient!!.start()
             }
         }
     }
