0908046416c41df3730a5bc95c6af5f2202f23f7
sshropshire <sshropshire@paypal.com>
2025-03-26 16:52:01 -0500

    Implement GooglePay presentment.

--- CorePayments/src/main/java/com/paypal/android/corepayments/GooglePayConfig.kt
+++ CorePayments/src/main/java/com/paypal/android/corepayments/GooglePayConfig.kt
@@ -1,5 +1,10 @@
 package com.paypal.android.corepayments
 
+import org.json.JSONArray
+import org.json.JSONObject
+
 data class GooglePayConfig(
     val isEligible: Boolean,
+    val allowedPaymentMethods: JSONArray,
+    val merchantInfo: JSONObject,
 )
--- Demo/src/main/java/com/paypal/android/ui/googlepay/GooglePayView.kt
+++ Demo/src/main/java/com/paypal/android/ui/googlepay/GooglePayView.kt
@@ -1,5 +1,7 @@
 package com.paypal.android.ui.googlepay
 
+import android.util.Log
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -11,19 +13,31 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.google.android.gms.wallet.contract.TaskResultContracts
 import com.paypal.android.uishared.components.StepHeader
 import com.paypal.android.utils.UIConstants
 import com.paypal.android.utils.getActivityOrNull
+import kotlinx.coroutines.launch
 
 @Composable
 fun GooglePayView(
     viewModel: GooglePayViewModel = hiltViewModel()
 ) {
+    // Ref: https://stackoverflow.com/a/67156998
+    val googlePayLauncher =
+        rememberLauncherForActivityResult(TaskResultContracts.GetPaymentDataResult()) { taskResult ->
+            Log.d("GooglePayView", "Result: $taskResult")
+        }
+
+    val context = LocalContext.current
+    val coroutineScope = rememberCoroutineScope()
+
     val scrollState = rememberScrollState()
     val contentPadding = UIConstants.paddingMedium
     Column(
@@ -33,21 +47,28 @@ fun GooglePayView(
             .padding(horizontal = contentPadding)
             .verticalScroll(scrollState)
     ) {
-        Step1_LaunchGooglePay(viewModel = viewModel)
+        Step1_LaunchGooglePay(
+            onLaunchGooglePay = {
+                context.getActivityOrNull()?.let { activity ->
+                    coroutineScope.launch {
+                        // Ref: https://developers.google.com/pay/api/android/guides/tutorial#initiate-payment
+                        val task = viewModel.launchGooglePay(activity)
+                        task.addOnCompleteListener(googlePayLauncher::launch)
+                    }
+                }
+            }
+        )
     }
 }
 
 @Composable
-private fun Step1_LaunchGooglePay(viewModel: GooglePayViewModel) {
-    val context = LocalContext.current
+private fun Step1_LaunchGooglePay(onLaunchGooglePay: () -> Unit) {
     Column(
         verticalArrangement = UIConstants.spacingMedium,
     ) {
         StepHeader(stepNumber = 1, title = "Create an Order")
         Button(
-            onClick = {
-                context.getActivityOrNull()?.let { viewModel.launchGooglePay(it) }
-            },
+            onClick = onLaunchGooglePay,
             modifier = Modifier
                 .fillMaxWidth()
         ) {
