771931e999e91f0150e99d72d12fb5ee698f7028
sshropshire <sshropshire@paypal.com>
2025-04-11 10:00:16 -0500

    Implement GooglePay create order flow.

---
+++ Demo/src/main/java/com/paypal/android/ui/googlepay/GooglePayUiState.kt
@@ -0,0 +1,15 @@
+package com.paypal.android.ui.googlepay
+
+import com.paypal.android.api.model.Order
+import com.paypal.android.api.model.OrderIntent
+import com.paypal.android.corepayments.ApproveGooglePayPaymentResult
+import com.paypal.android.uishared.state.ActionState
+
+data class GooglePayUiState(
+    val intentOption: OrderIntent = OrderIntent.AUTHORIZE,
+    val createOrderState: ActionState<Order, Exception> = ActionState.Idle,
+    val googlePayState: ActionState<ApproveGooglePayPaymentResult.Success, Exception> = ActionState.Idle,
+) {
+    val isCreateOrderSuccessful: Boolean
+        get() = createOrderState is ActionState.Success
+}
