51cf59c86d910ef15630bb2313ced5ba3213ecdc
sshropshire <sshropshire@paypal.com>
2025-04-15 13:05:44 -0500

    Implement accessibility proof of concept.

--- PaymentButtons/src/main/java/com/paypal/android/paymentbuttons/PayPalButtonV2.kt
+++ PaymentButtons/src/main/java/com/paypal/android/paymentbuttons/PayPalButtonV2.kt
@@ -3,10 +3,8 @@ package com.paypal.android.paymentbuttons
 import android.content.Context
 import android.util.AttributeSet
 import android.util.TypedValue
-import android.view.Gravity
-import androidx.appcompat.widget.AppCompatButton
-import androidx.core.content.ContextCompat
-import androidx.core.graphics.toColorInt
+import android.widget.Button
+import android.widget.RelativeLayout
 import com.paypal.android.ui.R
 
 // Ref: https://stackoverflow.com/a/6327095
@@ -20,15 +18,42 @@ class PayPalButtonV2 @JvmOverloads constructor(
     context: Context,
     attributeSet: AttributeSet? = null,
     defStyleAttr: Int = 0
-) : AppCompatButton(context, attributeSet, defStyleAttr) {
+) : RelativeLayout(context, attributeSet, defStyleAttr) {
 
     init {
-        text = "helloooo"
-        minHeight = context.toPx(48).toInt()
-        gravity = Gravity.CENTER
+        inflate(context, R.layout.paypal_button_v2, this)
+//        // Ref: https://stackoverflow.com/a/3177667
+//        val textPrefix = "Pay with ^"
+//        val spannableString = SpannableString(textPrefix)
+//        val alignmentSpan = AlignmentSpan.Standard(Layout.Alignment.ALIGN_CENTER)
+//        spannableString.setSpan(alignmentSpan, 0, "Pay with".length, Spannable.SPAN_INCLUSIVE_EXCLUSIVE)
+//
+//        val wordmarkDrawable = ContextCompat.getDrawable(context, R.drawable.wordmark_paypal_v2)!!
+//        wordmarkDrawable.setBounds(
+//            0,
+//            0,
+//            wordmarkDrawable.intrinsicWidth,
+//            wordmarkDrawable.intrinsicHeight
+//        )
+//        val imageSpan = ImageSpan(wordmarkDrawable, ImageSpan.ALIGN_BASELINE)
+//        spannableString.setSpan(
+//            imageSpan,
+//            textPrefix.length - 1,
+//            textPrefix.length,
+//            Spannable.SPAN_INCLUSIVE_EXCLUSIVE
+//        )
+//        text = "Pay with"
+//
+////        minHeight = context.toPx(48).toInt()
+//        gravity = Gravity.CENTER
+//
+//        // TODO: leverage material shape drawable to programmatically set corner radius and maintain ripple effect
+//        val drawable = ContextCompat.getDrawable(context, R.drawable.paypal_button_v2_background)
+//        background = drawable
+//        setCompoundDrawables(null, null, wordmarkDrawable, null)
+    }
 
-        // TODO: leverage material shape drawable to programmatically set corner radius and maintain ripple effect
-        val drawable = ContextCompat.getDrawable(context, R.drawable.paypal_button_v2_background)
-        background = drawable
+    override fun setOnClickListener(l: OnClickListener?) {
+        findViewById<Button>(R.id.button).setOnClickListener(l)
     }
 }
