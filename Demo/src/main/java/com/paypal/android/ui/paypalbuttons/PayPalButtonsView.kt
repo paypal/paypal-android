fe4319a8db012ab48771e64184c27f8c33a73dfc
sshropshire <sshropshire@paypal.com>
2025-05-06 11:47:46 -0500

    Remove unused imports.

--- Demo/src/main/java/com/paypal/android/ui/paypalbuttons/PayPalButtonsView.kt
+++ Demo/src/main/java/com/paypal/android/ui/paypalbuttons/PayPalButtonsView.kt
@@ -1,7 +1,6 @@
 package com.paypal.android.ui.paypalbuttons
 
 import android.content.Context
-import android.view.ViewGroup
 import android.widget.Toast
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
@@ -26,7 +25,6 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
