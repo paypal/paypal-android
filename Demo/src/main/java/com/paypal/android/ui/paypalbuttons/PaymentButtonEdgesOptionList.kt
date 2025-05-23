d05529d850139fe9039d556e7195cdff83126755
sshropshire <sshropshire@paypal.com>
2025-05-23 13:46:53 -0500

    Implement corner radius slider.

--- Demo/src/main/java/com/paypal/android/ui/paypalbuttons/PaymentButtonEdgesOptionList.kt
+++ Demo/src/main/java/com/paypal/android/ui/paypalbuttons/PaymentButtonEdgesOptionList.kt
@@ -1,6 +1,7 @@
 package com.paypal.android.ui.paypalbuttons
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.selection.selectableGroup
 import androidx.compose.runtime.Composable
@@ -9,6 +10,7 @@ import androidx.compose.material3.Card
 import androidx.compose.material3.Divider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.SliderDefaults
+import androidx.compose.material3.Text
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import com.paypal.android.R
@@ -65,24 +67,36 @@ fun PaymentButtonEdgesOptionList(
                     }
                 }
             )
-            val cornerRadius = (edges as? PaymentButtonEdges.Custom)?.cornerRadius ?: 0
-            IntSlider(
-                value = cornerRadius.toInt(),
-                valueRange = 0..CORNER_RADIUS_SLIDER_MAX,
-                steps = CORNER_RADIUS_SLIDER_MAX,
-                colors = SliderDefaults.colors(
-                    inactiveTrackColor = MaterialTheme.colorScheme.onSurfaceVariant,
-                ),
-                onValueChange = { value ->
-                    val newSelection = PaymentButtonEdges.Custom(value.toFloat())
-                    onSelection(newSelection)
-                },
+            Column(
                 modifier = Modifier.padding(
                     start = UIConstants.paddingMedium,
                     end = UIConstants.paddingMedium,
                     bottom = UIConstants.paddingMedium
                 )
-            )
+            ) {
+                val cornerRadius = (edges as? PaymentButtonEdges.Custom)?.cornerRadius ?: 0
+                Text(
+                    text = "${cornerRadius.toInt()}px",
+                    color = MaterialTheme.colorScheme.onSurfaceVariant,
+                    style = MaterialTheme.typography.titleLarge,
+                    modifier = Modifier
+                        .padding(bottom = UIConstants.paddingSmall)
+                        .fillMaxWidth()
+                )
+                IntSlider(
+                    value = cornerRadius.toInt(),
+                    valueRange = 0..CORNER_RADIUS_SLIDER_MAX,
+                    steps = CORNER_RADIUS_SLIDER_MAX,
+                    colors = SliderDefaults.colors(
+                        inactiveTrackColor = MaterialTheme.colorScheme.onSurfaceVariant,
+                    ),
+                    onValueChange = { value ->
+                        val newSelection = PaymentButtonEdges.Custom(value.toFloat())
+                        onSelection(newSelection)
+                    }
+                )
+
+            }
         }
     }
 }
