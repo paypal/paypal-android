906fabb41960b1997c713e69ebedfce0f7c286bd
sshropshire <sshropshire@paypal.com>
2025-05-23 14:00:09 -0500

    Fix lint errors.

--- Demo/src/main/java/com/paypal/android/ui/paypalbuttons/PaymentButtonEdgesOptionList.kt
+++ Demo/src/main/java/com/paypal/android/ui/paypalbuttons/PaymentButtonEdgesOptionList.kt
@@ -4,16 +4,16 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.selection.selectableGroup
-import androidx.compose.runtime.Composable
-import com.paypal.android.paymentbuttons.PaymentButtonEdges
 import androidx.compose.material3.Card
 import androidx.compose.material3.Divider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.SliderDefaults
 import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import com.paypal.android.R
+import com.paypal.android.paymentbuttons.PaymentButtonEdges
 import com.paypal.android.uishared.components.IntSlider
 import com.paypal.android.uishared.components.OptionListItem
 import com.paypal.android.uishared.components.OptionListTitle
@@ -66,37 +66,44 @@ fun PaymentButtonEdgesOptionList(
                     }
                 }
             )
-            Column(
-                modifier = Modifier.padding(
-                    start = UIConstants.paddingMedium,
-                    end = UIConstants.paddingMedium,
-                    bottom = UIConstants.paddingMedium
-                )
-            ) {
-                val cornerRadius = (edges as? PaymentButtonEdges.Custom)?.cornerRadius ?: 0
-                Text(
-                    text = "${cornerRadius.toInt()}px",
-                    color = MaterialTheme.colorScheme.onSurfaceVariant,
-                    style = MaterialTheme.typography.titleLarge,
-                    modifier = Modifier
-                        .padding(bottom = UIConstants.paddingSmall)
-                        .fillMaxWidth()
-                )
-                IntSlider(
-                    value = cornerRadius.toInt(),
-                    valueRange = 0..CORNER_RADIUS_SLIDER_MAX,
-                    steps = CORNER_RADIUS_SLIDER_MAX,
-                    colors = SliderDefaults.colors(
-                        inactiveTrackColor = MaterialTheme.colorScheme.onSurfaceVariant,
-                    ),
-                    onValueChange = { value ->
-                        val newSelection = PaymentButtonEdges.Custom(value.toFloat())
-                        onSelection(newSelection)
-                    }
-                )
-
-            }
+            PaymentButtonEdgesCustomRadiusSlider(edges, onSelection = onSelection)
         }
+    }
+}
+
+@Composable
+fun PaymentButtonEdgesCustomRadiusSlider(
+    edges: PaymentButtonEdges,
+    onSelection: (PaymentButtonEdges) -> Unit
+) {
+    Column(
+        modifier = Modifier.padding(
+            start = UIConstants.paddingMedium,
+            end = UIConstants.paddingMedium,
+            bottom = UIConstants.paddingMedium
+        )
+    ) {
+        val cornerRadius = (edges as? PaymentButtonEdges.Custom)?.cornerRadius ?: 0
+        Text(
+            text = "${cornerRadius.toInt()}px",
+            color = MaterialTheme.colorScheme.onSurfaceVariant,
+            style = MaterialTheme.typography.titleLarge,
+            modifier = Modifier
+                .padding(bottom = UIConstants.paddingSmall)
+                .fillMaxWidth()
+        )
+        IntSlider(
+            value = cornerRadius.toInt(),
+            valueRange = 0..CORNER_RADIUS_SLIDER_MAX,
+            steps = CORNER_RADIUS_SLIDER_MAX,
+            colors = SliderDefaults.colors(
+                inactiveTrackColor = MaterialTheme.colorScheme.onSurfaceVariant,
+            ),
+            onValueChange = { value ->
+                val newSelection = PaymentButtonEdges.Custom(value.toFloat())
+                onSelection(newSelection)
+            }
+        )
     }
 }
 
@@ -109,12 +116,3 @@ fun edgesToString(edges: PaymentButtonEdges): String {
     }
     return option.name
 }
-
-//fun stringToEdges(value: String): PaymentButtonEdges {
-//    val edgesOption = PaymentButtonEdgesOption.valueOf(value)
-//    return when (edgesOption) {
-//        PaymentButtonEdgesOption.SOFT -> PaymentButtonEdges.Soft
-//        PaymentButtonEdgesOption.PILL -> PaymentButtonEdges.Pill
-//        PaymentButtonEdgesOption.SHARP -> PaymentButtonEdges.Sharp
-//    }
-//}
