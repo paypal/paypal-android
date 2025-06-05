bd26bb573db19dac8ca80289da4f6705bc920a10
sshropshire <sshropshire@paypal.com>
2025-06-05 10:42:09 -0500

    Clean up PayPalButtonsView.

--- Demo/src/main/java/com/paypal/android/ui/paypalbuttons/PayPalButtonsView.kt
+++ Demo/src/main/java/com/paypal/android/ui/paypalbuttons/PayPalButtonsView.kt
@@ -6,11 +6,8 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -22,7 +19,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -100,9 +96,7 @@ fun PayPalButtonsView(viewModel: PayPalButtonsViewModel = viewModel()) {
 
 @Composable
 fun PayPalButtonFactory(uiState: PayPalButtonsUiState) {
-    val buttonModifier: Modifier = Modifier.wrapContentSize()
-//    val buttonModifier: Modifier = Modifier.height(25.dp)
-
+    val buttonModifier: Modifier = Modifier.fillMaxWidth()
     when (uiState.fundingType) {
         ButtonFundingType.PAYPAL -> {
             AndroidView(
