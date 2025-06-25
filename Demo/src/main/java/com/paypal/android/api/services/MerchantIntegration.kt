b28c538c1ba237cefb4b5c81497ee0776e13530f
sshropshire <sshropshire@paypal.com>
2025-06-25 14:46:15 -0500

    Fix lint error.

--- Demo/src/main/java/com/paypal/android/api/services/MerchantIntegration.kt
+++ Demo/src/main/java/com/paypal/android/api/services/MerchantIntegration.kt
@@ -6,4 +6,3 @@ enum class MerchantIntegration(val baseUrl: String, val clientId: String) {
         clientId = "AQTfw2irFfemo-eWG4H5UY-b9auKihUpXQ2Engl4G1EsHJe2mkpfUv_SN3Mba0v3CfrL6Fk_ecwv9EOo"
     ),
 }
-
