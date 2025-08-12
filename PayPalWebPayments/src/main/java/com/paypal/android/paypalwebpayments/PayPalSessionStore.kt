9f9c182135741fb721e2c14aeb19bb86f0af4e80
sshropshire <sshropshire@paypal.com>
2025-08-12 11:18:27 -0500

    Tweak PayPalSessionStore visibility qualifiers.

--- PayPalWebPayments/src/main/java/com/paypal/android/paypalwebpayments/PayPalSessionStore.kt
+++ PayPalWebPayments/src/main/java/com/paypal/android/paypalwebpayments/PayPalSessionStore.kt
@@ -3,9 +3,9 @@ package com.paypal.android.paypalwebpayments
 import com.paypal.android.corepayments.SessionStore
 
 internal class PayPalSessionStore() : SessionStore() {
-    internal var authState: String? by properties
+    var authState: String? by properties
 
     // for analytics tracking
-    internal var checkoutOrderId: String? by properties
-    internal var vaultSetupTokenId: String? by properties
+    var checkoutOrderId: String? by properties
+    var vaultSetupTokenId: String? by properties
 }
