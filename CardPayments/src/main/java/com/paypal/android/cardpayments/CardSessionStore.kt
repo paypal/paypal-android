3f7b2eb6a4f85f9e3760c31201b2148f15abea1b
sshropshire <sshropshire@paypal.com>
2025-09-05 10:54:16 -0500

    Implement instance state restoration in CardClient.

--- CardPayments/src/main/java/com/paypal/android/cardpayments/CardSessionStore.kt
+++ CardPayments/src/main/java/com/paypal/android/cardpayments/CardSessionStore.kt
@@ -1,5 +1,8 @@
 package com.paypal.android.cardpayments
 
+import com.paypal.android.corepayments.restoreFromBase64EncodedJSON
+import com.paypal.android.corepayments.toBase64EncodedJSON
+
 private const val KEY_AUTH_STATE = "Card.AUTH_STATE"
 
 internal class CardSessionStore {
@@ -12,4 +15,9 @@ internal class CardSessionStore {
         }
 
     fun clear() = properties.clear()
+
+    fun restore(base64EncodedJSON: String) =
+        properties.restoreFromBase64EncodedJSON(base64EncodedJSON)
+
+    fun toBase64EncodedJSON() = properties.toBase64EncodedJSON()
 }
