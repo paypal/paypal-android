48ee01c17f347af7dafb0fa32828d6d9e27a1adc
Karthik Gangineni <kgangineni@paypal.com>
2025-09-11 19:21:19 -0500

    change extensions to generic JSONObject

--- CorePayments/src/main/java/com/paypal/android/corepayments/graphql/GraphQLResponse.kt
+++ CorePayments/src/main/java/com/paypal/android/corepayments/graphql/GraphQLResponse.kt
@@ -3,12 +3,13 @@ package com.paypal.android.corepayments.graphql
 import androidx.annotation.RestrictTo
 import kotlinx.serialization.InternalSerializationApi
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.JsonObject
 
 @RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
 @InternalSerializationApi
 @Serializable
 data class GraphQLResponse<out T> @OptIn(InternalSerializationApi::class) constructor(
     val data: T? = null,
-    val extensions: List<GraphQLExtension>? = null,
+    val extensions: JsonObject? = null,
     val errors: List<GraphQLError>? = null,
 )
