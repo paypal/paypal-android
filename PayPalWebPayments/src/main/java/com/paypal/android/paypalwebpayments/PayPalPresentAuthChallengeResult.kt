8975119efe37f221d0a01316c21542e26daf80e7
sshropshire <sshropshire@paypal.com>
2025-09-19 14:19:07 -0500

    Revert changes to authState property that broke binary api compatability.

--- PayPalWebPayments/src/main/java/com/paypal/android/paypalwebpayments/PayPalPresentAuthChallengeResult.kt
+++ PayPalWebPayments/src/main/java/com/paypal/android/paypalwebpayments/PayPalPresentAuthChallengeResult.kt
@@ -3,10 +3,11 @@ package com.paypal.android.paypalwebpayments
 import com.paypal.android.corepayments.PayPalSDKError
 
 sealed class PayPalPresentAuthChallengeResult {
-    data class Success(internal val authStateInternal: String) : PayPalPresentAuthChallengeResult() {
+    data class Success(
         // TODO: v3 – remove public facing authState property
         @Deprecated("This accessor is no longer supported. Auth state is now managed internally by the SDK.")
-        val authState: String = authStateInternal
-    }
+        internal val authState: String
+    ) : PayPalPresentAuthChallengeResult()
+
     data class Failure(val error: PayPalSDKError) : PayPalPresentAuthChallengeResult()
 }
