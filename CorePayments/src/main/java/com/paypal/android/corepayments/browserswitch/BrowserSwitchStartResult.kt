ea357bcb98f9bb167a4ed48f86a1a77480a2ec57
sshropshire <sshropshire@paypal.com>
2025-12-08 11:22:28 -0600

    Fix detekt errors.

--- CorePayments/src/main/java/com/paypal/android/corepayments/browserswitch/BrowserSwitchStartResult.kt
+++ CorePayments/src/main/java/com/paypal/android/corepayments/browserswitch/BrowserSwitchStartResult.kt
@@ -1,6 +1,6 @@
 package com.paypal.android.corepayments.browserswitch
 
-sealed class BrowserSwitchStartResult() {
-    object Success: BrowserSwitchStartResult()
-    class Failure(val error: Exception): BrowserSwitchStartResult()
+sealed class BrowserSwitchStartResult {
+    object Success : BrowserSwitchStartResult()
+    class Failure(val error: Exception) : BrowserSwitchStartResult()
 }
