7e24c28afb4fda8424fbb91d172f12610b118b94
sshropshire <sshropshire@paypal.com>
2025-12-10 12:43:06 -0600

    Add comment to BrowserSwitchRequestCodes.

--- CorePayments/src/main/java/com/paypal/android/corepayments/BrowserSwitchRequestCodes.kt
+++ CorePayments/src/main/java/com/paypal/android/corepayments/BrowserSwitchRequestCodes.kt
@@ -1,5 +1,9 @@
 package com.paypal.android.corepayments
 
+// TODO: consider breaking change to migrate away from a centralized set of request codes
+// to module-specific request keys to prevent each module from having to depend on :Core
+// for its request codes; there will be risk of collision, but proper namespacing can alleviate this
+// concern e.g. "PayPal.Checkout", "Venmo.Vault" etc.
 object BrowserSwitchRequestCodes {
     const val CARD_APPROVE_ORDER = 1
     const val CARD_VAULT = 2
