5beb3da7fe5b442e797f4cb5a459371ed74da609
Karthik Gangineni <kgangineni@paypal.com>
2025-12-17 14:39:43 -0600

    revert demo constants

--- Demo/src/main/java/com/paypal/android/DemoConstants.kt
+++ Demo/src/main/java/com/paypal/android/DemoConstants.kt
@@ -1,11 +1,10 @@
 package com.paypal.android
 
 object DemoConstants {
-    // Made these temporary changes because getting Not verified error when using applink with auth tab
-    const val APP_URL = "com.paypal.android.demo://"
+    const val APP_URL = "https://ppcp-mobile-demo-sandbox-87bbd7f0a27f.herokuapp.com"
     const val APP_FALLBACK_URL_SCHEME = "com.paypal.android.demo"
-    const val SUCCESS_URL = "com.paypal.android.demo://success"
-    const val CANCEL_URL = "com.paypal.android.demo://cancel"
+    const val SUCCESS_URL = "$APP_URL/success"
+    const val CANCEL_URL = "$APP_URL/cancel"
     const val VAULT_SUCCESS_URL = "$APP_URL/vault/success"
     const val VAULT_CANCEL_URL = "$APP_URL/vault/cancel"
 }
