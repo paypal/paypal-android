1653e1ad9dc4423e247537fa6b772b3a628ff95b
sshropshire <sshropshire@paypal.com>
2026-05-01 15:24:56 -0500

    Remove unnecessary file.

--- CorePayments/src/main/java/com/paypal/android/corepayments/ApproveGooglePayPaymentResult.kt
+++
@@ -1,6 +0,0 @@
-package com.paypal.android.corepayments
-
-sealed class ApproveGooglePayPaymentResult {
-    data class Success(val status: String) : ApproveGooglePayPaymentResult()
-    data class Failure(val error: PayPalSDKError) : ApproveGooglePayPaymentResult()
-}
