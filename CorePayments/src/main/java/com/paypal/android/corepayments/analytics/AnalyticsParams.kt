3047ada72f19a4061fb3dc8dd0c2a70a700b40e2
Anibal Bojorquez <anbojorquez@paypal.com>
2026-06-03 19:02:50 -0700

    Updating Analytics to use maps

---
+++ CorePayments/src/main/java/com/paypal/android/corepayments/analytics/AnalyticsParams.kt
@@ -0,0 +1,13 @@
+package com.paypal.android.corepayments.analytics
+
+import androidx.annotation.RestrictTo
+
+/**
+ * Well-known FPTI parameter keys used when building analytics event param maps.
+ * Each analytics component references these constants to ensure consistent key naming.
+ */
+@RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
+object AnalyticsParams {
+    const val ORDER_ID = "order_id"
+    const val BUTTON_TYPE = "button_type"
+}
