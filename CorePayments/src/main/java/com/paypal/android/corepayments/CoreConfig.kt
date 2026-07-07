8cee1289da53c5f7582121cb3c68a2595939e61c
Anibal Bojorquez <anbojorquez@paypal.com>
2026-07-07 12:27:02 -0700

    removing deprecated CoreConfig constructors

--- CorePayments/src/main/java/com/paypal/android/corepayments/CoreConfig.kt
+++ CorePayments/src/main/java/com/paypal/android/corepayments/CoreConfig.kt
@@ -5,17 +5,4 @@ data class CoreConfig @JvmOverloads constructor(
     val merchantId: String,
     val environment: Environment = Environment.SANDBOX,
     val bnCode: String? = null,
-) {
-
-    @Deprecated(
-        message = "Provide merchantId. Use CoreConfig(clientId, merchantId) instead.",
-        level = DeprecationLevel.WARNING
-    )
-    constructor(clientId: String) : this(clientId, "")
-
-    @Deprecated(
-        message = "Provide merchantId. Use CoreConfig(clientId, merchantId, environment) instead.",
-        level = DeprecationLevel.WARNING
-    )
-    constructor(clientId: String, environment: Environment) : this(clientId, "", environment)
-}
+)
