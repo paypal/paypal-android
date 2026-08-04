c14e9c065ad565ecca1ff8e5cc117888077b85d2
Anibal Bojorquez <anbojorquez@paypal.com>
2026-08-03 17:18:16 -0700

    correcting pay-pal-payments to paypal-payments

--- PayPalPayments/src/main/java/com/paypal/android/paypalpayments/analytics/PayPalEvent.kt
+++ PayPalPayments/src/main/java/com/paypal/android/paypalpayments/analytics/PayPalEvent.kt
@@ -4,23 +4,23 @@ package com.paypal.android.paypalpayments.analytics
 
 internal enum class PayPalEvent(val value: String) {
     // @formatter:off
-    STARTED("pay-pal-payments:checkout:started"),
-    SUCCEEDED("pay-pal-payments:checkout:succeeded"),
-    FAILED("pay-pal-payments:checkout:failed"),
-    CANCELED( "pay-pal-payments:checkout:canceled"),
+    STARTED("paypal-payments:checkout:started"),
+    SUCCEEDED("paypal-payments:checkout:succeeded"),
+    FAILED("paypal-payments:checkout:failed"),
+    CANCELED( "paypal-payments:checkout:canceled"),
 
-    SESSION_NOT_STARTED("pay-pal-payments:checkout:session-not-started"),
+    SESSION_NOT_STARTED("paypal-payments:checkout:session-not-started"),
 
-    APP_SWITCH_STARTED("pay-pal-payments:checkout:app-switch:started"),
-    APP_SWITCH_SUCCEEDED("pay-pal-payments:checkout:app-switch:succeeded"),
-    APP_SWITCH_FAILED("pay-pal-payments:checkout:app-switch:failed"),
+    APP_SWITCH_STARTED("paypal-payments:checkout:app-switch:started"),
+    APP_SWITCH_SUCCEEDED("paypal-payments:checkout:app-switch:succeeded"),
+    APP_SWITCH_FAILED("paypal-payments:checkout:app-switch:failed"),
 
-    AUTH_CHALLENGE_PRESENTATION_STARTED("pay-pal-payments:checkout:auth-challenge-presentation:started"),
-    AUTH_CHALLENGE_PRESENTATION_SUCCEEDED("pay-pal-payments:checkout:auth-challenge-presentation:succeeded"),
-    AUTH_CHALLENGE_PRESENTATION_FAILED("pay-pal-payments:checkout:auth-challenge-presentation:failed"),
+    AUTH_CHALLENGE_PRESENTATION_STARTED("paypal-payments:checkout:auth-challenge-presentation:started"),
+    AUTH_CHALLENGE_PRESENTATION_SUCCEEDED("paypal-payments:checkout:auth-challenge-presentation:succeeded"),
+    AUTH_CHALLENGE_PRESENTATION_FAILED("paypal-payments:checkout:auth-challenge-presentation:failed"),
 
-    HANDLE_RETURN_STARTED("pay-pal-payments:checkout:handle-return:started"),
-    HANDLE_RETURN_SUCCEEDED("pay-pal-payments:checkout:handle-return:succeeded"),
-    HANDLE_RETURN_FAILED("pay-pal-payments:checkout:handle-return:failed"),
+    HANDLE_RETURN_STARTED("paypal-payments:checkout:handle-return:started"),
+    HANDLE_RETURN_SUCCEEDED("paypal-payments:checkout:handle-return:succeeded"),
+    HANDLE_RETURN_FAILED("paypal-payments:checkout:handle-return:failed"),
     // @formatter:on
 }
